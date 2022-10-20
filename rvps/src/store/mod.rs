842046a84d8630808e9c574ca9dd4d427dbdeb4c
Xynnn007 <xynnn@linux.alibaba.com>
2022-10-20 21:35:25 +0800

    fix: rename cache to store in RVPS
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- rvps/src/store/mod.rs
+++ rvps/src/store/mod.rs
@@ -3,18 +3,18 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-//! Cache is responsible for storing verified Reference Values
+//! Store is responsible for storing verified Reference Values
 
 use crate::reference_value::ReferenceValue;
 
 use anyhow::Result;
 
-/// Interface of a `Cache`.
+/// Interface of a `Store`.
 /// We only provide a simple instance here which implements
-/// Cache. In more scenarios, RV should be stored in persistent
+/// Store. In more scenarios, RV should be stored in persistent
 /// storage, like database, file and so on. All of the mentioned
 /// forms will have the same interface as following.
-pub trait Cache {
+pub trait Store {
     /// Store a reference value
     fn set(&mut self, name: String, rv: ReferenceValue) -> Result<Option<ReferenceValue>>;
 
