8fff1b8ffb19e07ae84d0b2719e9f726f12cd4c9
Jiale Zhang <zhangjiale@linux.alibaba.com>
2022-10-21 05:54:25 -0400

    AS: Modular policy engine to support alternative policy engines.
    1. Abstract the calling interface of the policy engine to support other policy engine instances.
    
    2. Add Config module specify the type of policy engine.
    
    Signed-off-by: Jiale Zhang <zhangjiale@linux.alibaba.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,53 +8,48 @@ extern crate strum;
 #[macro_use]
 extern crate strum_macros;
 
+pub mod config;
 pub mod policy_engine;
 pub mod types;
 pub mod verifier;
 
-use anyhow::{Context, Result};
-use policy_engine::opa;
-use serde_json::Value;
+use anyhow::{anyhow, Context, Result};
+use config::Config;
+use policy_engine::{PolicyEngine, PolicyEngineType};
 use std::collections::HashMap;
+use std::fs;
 use std::str::FromStr;
 use types::{AttestationResults, Evidence, TEE};
 
-#[macro_export]
-macro_rules! default_policy {
-    () => {
-        "../default_policy.rego"
-    };
-}
-
-#[derive(Debug, Default)]
+#[allow(dead_code)]
 pub struct AttestationService {
-    policy: String,
-    reference_data: String,
+    config: Config,
+    policy_engine: Box<dyn PolicyEngine + Send + Sync>,
+    reference_values: String,
 }
 
 impl AttestationService {
     /// Create a new Attestation Service instance.
     pub fn new() -> Result<Self> {
-        let policy = std::include_str!(default_policy!()).to_string();
+        let config = Config::default();
+        if !config.work_dir.as_path().exists() {
+            fs::create_dir_all(&config.work_dir)
+                .map_err(|e| anyhow!("Create AS work dir failed: {:?}", e))?;
+        }
+
+        let policy_engine = PolicyEngineType::from_str(&config.policy_engine)
+            .map_err(|_| anyhow!("Policy Engine {} is not supported", &config.policy_engine))?
+            .to_policy_engine(config.work_dir.as_path())?;
 
         let data: HashMap<String, Vec<String>> = HashMap::new();
-        //TODO: Reference data actually should be generated from RVPS storage when `evaluate` is called.
-        let reference_data = serde_json::json!({ "reference": data }).to_string();
+        //TODO: Reference values actually should be generated from RVPS storage when `evaluate` is called.
+        let reference_values = serde_json::json!({ "reference": data }).to_string();
 
         Ok(Self {
-            policy,
-            reference_data,
+            config,
+            policy_engine,
+            reference_values,
         })
-    }
-
-    /// Get current policy.
-    pub fn policy(&self) -> String {
-        self.policy.clone()
-    }
-
-    /// Set customize policy.
-    pub fn set_policy(&mut self, policy: String) {
-        self.policy = policy;
     }
 
     /// Evaluate Attestation Evicende.
@@ -81,18 +76,16 @@ impl AttestationService {
             }
         };
 
-        let opa_output = opa::evaluate(
-            self.policy.clone(),
-            self.reference_data.clone(),
+        let (result, policy_engine_output) = self.policy_engine.evaluate(
+            self.reference_values.clone(),
             serde_json::to_string(&claims_from_tee_evidence)?,
         )?;
-        let v_opa_output: Value = serde_json::from_str(&opa_output)?;
 
         let attestation_results = AttestationResults::new(
             tee.to_string(),
-            v_opa_output["allow"].as_bool().unwrap_or(false),
+            result,
             None,
-            Some(opa_output),
+            Some(policy_engine_output),
             Some(serde_json::to_string(&claims_from_tee_evidence)?),
         );
         debug!("Attestation Results: {:?}", &attestation_results);
--- src/policy_engine/opa/mod.rs
+++ src/policy_engine/opa/mod.rs
@@ -1,6 +1,12 @@
+use crate::policy_engine::PolicyEngine;
 use anyhow::{anyhow, Result};
+use serde_json::Value;
 use std::ffi::CStr;
+use std::fs;
 use std::os::raw::c_char;
+use std::path::PathBuf;
+
+const POLICY_FILE: &str = "opa/policy.rego";
 
 // Link import cgo function
 #[link(name = "opa")]
@@ -16,37 +22,66 @@ pub struct GoString {
     pub n: isize,
 }
 
-pub fn evaluate(policy: String, reference: String, input: String) -> Result<String> {
-    let policy_go = GoString {
-        p: policy.as_ptr() as *const i8,
-        n: policy.len() as isize,
-    };
+#[derive(Debug)]
+pub struct OPA {
+    policy_file_path: PathBuf,
+}
 
-    let reference_go = GoString {
-        p: reference.as_ptr() as *const i8,
-        n: reference.len() as isize,
-    };
+impl OPA {
+    pub fn new(work_dir: PathBuf) -> Result<Self> {
+        let mut policy_file_path = work_dir;
+        policy_file_path.push(POLICY_FILE);
 
-    let input_go = GoString {
-        p: input.as_ptr() as *const i8,
-        n: input.len() as isize,
-    };
+        if !policy_file_path.as_path().exists() {
+            fs::create_dir_all(&policy_file_path)
+                .map_err(|e| anyhow!("Create policy dir failed: {:?}", e))?;
+            let policy = std::include_str!("default_policy.rego").to_string();
+            fs::write(&policy_file_path, policy)?;
+        }
 
-    // Call the function exported by cgo and process the returned decision
-    let decision_buf: *mut c_char = unsafe { evaluateGo(policy_go, reference_go, input_go) };
-    let decision_str: &CStr = unsafe { CStr::from_ptr(decision_buf) };
-    let res = decision_str.to_str()?.to_string();
-    debug!("Evaluated: {}", res);
-    if res.starts_with("Error::") {
-        return Err(anyhow!(res));
+        Ok(Self { policy_file_path })
     }
-    Ok(res)
+}
+
+impl PolicyEngine for OPA {
+    fn evaluate(&self, reference: String, input: String) -> Result<(bool, String)> {
+        let policy = fs::read_to_string(&self.policy_file_path)
+            .map_err(|e| anyhow!("Read OPA policy file failed: {:?}", e))?;
+
+        let policy_go = GoString {
+            p: policy.as_ptr() as *const i8,
+            n: policy.len() as isize,
+        };
+
+        let reference_go = GoString {
+            p: reference.as_ptr() as *const i8,
+            n: reference.len() as isize,
+        };
+
+        let input_go = GoString {
+            p: input.as_ptr() as *const i8,
+            n: input.len() as isize,
+        };
+
+        // Call the function exported by cgo and process the returned decision
+        let decision_buf: *mut c_char = unsafe { evaluateGo(policy_go, reference_go, input_go) };
+        let decision_str: &CStr = unsafe { CStr::from_ptr(decision_buf) };
+        let res = decision_str.to_str()?.to_string();
+        debug!("Evaluated: {}", res);
+        if res.starts_with("Error::") {
+            return Err(anyhow!(res));
+        }
+
+        let res_kv: Value = serde_json::from_str(&res)?;
+
+        Ok((res_kv["allow"].as_bool().unwrap_or(false), res))
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use serde_json::{json, Value};
+    use serde_json::json;
 
     fn dummy_reference(ver: u64) -> String {
         json!({
@@ -68,16 +103,16 @@ mod tests {
 
     #[test]
     fn test_evaluate() {
-        let policy = std::include_str!("../../../default_policy.rego").to_string();
+        let opa = OPA {
+            policy_file_path: PathBuf::from("./src/policy_engine/opa/default_policy.rego"),
+        };
 
-        let res = evaluate(policy.clone(), dummy_reference(5), dummy_input(5, 5));
+        let res = opa.evaluate(dummy_reference(5), dummy_input(5, 5));
         assert!(res.is_ok(), "OPA execution() should be success");
-        let v: Value = serde_json::from_str(&res.unwrap()).unwrap();
-        assert!(v["allow"] == true, "allow should be true");
+        assert!(res.unwrap().0 == true, "allow should be true");
 
-        let res = evaluate(policy.clone(), dummy_reference(5), dummy_input(0, 0));
+        let res = opa.evaluate(dummy_reference(5), dummy_input(0, 0));
         assert!(res.is_ok(), "OPA execution() should be success");
-        let v: Value = serde_json::from_str(&res.unwrap()).unwrap();
-        assert!(v["allow"] == false, "allow should be false");
+        assert!(res.unwrap().0 == false, "allow should be false");
     }
 }
