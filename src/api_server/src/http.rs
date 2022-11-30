4026e845055ee6ea8e4cd0a90195698889f4b4b1
Samuel Ortiz <sameo@rivosinc.com>
2022-11-30 14:19:36 +0100

    api_server: Prepare and return a Challenge nonce
    Signed-off-by: Samuel Ortiz <sameo@rivosinc.com>

--- src/api_server/src/http.rs
+++ src/api_server/src/http.rs
@@ -1,12 +1,37 @@
-use actix_web::{web, HttpResponse};
+// Copyright (c) 2022 by Rivos Inc.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use actix_web::{body::BoxBody, web, HttpResponse};
+use anyhow::Result;
 use kbs_types::{Challenge, Request};
+use rand::{thread_rng, Rng};
+
+fn nonce() -> Result<String> {
+    let mut nonce: Vec<u8> = vec![0; 32];
+
+    thread_rng()
+        .try_fill(&mut nonce[..])
+        .map_err(anyhow::Error::from)?;
+
+    Ok(base64::encode_config(&nonce, base64::STANDARD))
+}
 
 /// This handler uses json extractor
 pub(crate) async fn auth(request: web::Json<Request>) -> HttpResponse {
-    println!("request: {:?}", &request);
+    log::info!("request: {:?}", &request);
+
+    let nonce = match nonce() {
+        Ok(n) => n,
+        Err(err) => {
+            return HttpResponse::InternalServerError()
+                .message_body(BoxBody::new(format!("{}", err)))
+                .unwrap();
+        }
+    };
 
     HttpResponse::Ok().json(Challenge {
-        nonce: "nonce".to_string(),
+        nonce,
         extra_params: "extra_params".to_string(),
     })
 }
