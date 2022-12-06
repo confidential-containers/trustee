41e636571c88dcc429fbda98ed4ed7bbe21807a6
Samuel Ortiz <sameo@rivosinc.com>
2022-12-06 10:45:51 +0100

    kbs: Add a --socket option
    Signed-off-by: Samuel Ortiz <sameo@rivosinc.com>

--- src/kbs/src/main.rs
+++ src/kbs/src/main.rs
@@ -9,19 +9,25 @@ extern crate anyhow;
 use anyhow::Result;
 use api_server::ApiServer;
 use attestation_service::AttestationService;
-use std::net::{IpAddr, SocketAddr};
+use std::net::SocketAddr;
 use std::sync::Arc;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Socket address (IP:port) to listen to, e.g. 127.0.0.1:8080.
+    /// This can be set multiple times.
+    #[arg(required = true, short, long)]
+    socket: Vec<SocketAddr>,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    let api_server = ApiServer::new(
-        vec![SocketAddr::from((
-            "127.0.0.1".parse::<IpAddr>().unwrap(),
-            8080,
-        ))],
-        Arc::new(AttestationService::new()?),
-    );
+    let cli = Cli::parse();
+    let api_server = ApiServer::new(cli.socket, Arc::new(AttestationService::new()?));
     api_server.serve().await.map_err(anyhow::Error::from)
 }
