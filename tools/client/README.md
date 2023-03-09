fc10880c6a63716fa85bb747779ef76ea427c56b
Jiale Zhang <zhangjiale@linux.alibaba.com>
2023-03-09 01:45:01 -0500

    Update Client Tool to adapt KBS resource URI
    Updated the Client Tool to use the KBS resource URI format defined by the Attention Agent.
    Document: https://github.com/confidential-containers/attestation-agent/blob/main/docs/KBS_URI.md
    
    Signed-off-by: Jiale Zhang <zhangjiale@linux.alibaba.com>

--- tools/client/README.md
+++ tools/client/README.md
@@ -14,8 +14,10 @@ For help:
 Request resource from KBS:
 
 ```shell
-./client --kbs-url http:/127.0.0.1:8080 --resource-path <repository>/<type>/<tag>
+./client --resource-uri kbs://127.0.0.1:8080/<repository>/<type>/<tag>
 ```
+
+Where the format of [KBS Resource URI](https://github.com/confidential-containers/attestation-agent/blob/main/docs/KBS_URI.md) is defined by Attestation Agent.
 
 Make sure this client run inside a real TEE which [Attestation Agent](https://github.com/confidential-containers/attestation-agent) supported (otherwise attestation will failed).
 
--- tools/client/src/main.rs
+++ tools/client/src/main.rs
@@ -8,27 +8,16 @@ use anyhow::Result;
 use attestation_agent::AttestationAPIs;
 use attestation_agent::AttestationAgent;
 use clap::Parser;
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 
 const CC_KBC_NAME: &str = "cc_kbc";
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// KBS URL. e.g: http://127.0.0.1:8080
+    /// KBS Resource URi. e.g: kbs://127.0.0.1:8080/my_repo/resource_type/123abc
+    /// Document: https://github.com/confidential-containers/attestation-agent/blob/main/docs/KBS_URI.md
     #[arg(required = true, long)]
-    kbs_url: String,
-
-    /// Resource path. e.g: "my_repo/resource_type/123abc"
-    #[arg(required = true, short, long)]
-    resource_path: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct ResourceDescription {
-    pub name: String,
-    pub optional: HashMap<String, String>,
+    resource_uri: String,
 }
 
 #[tokio::main]
@@ -36,26 +25,11 @@ async fn main() -> Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     let cli = Cli::parse();
-    let resource_path: Vec<&str> = cli.resource_path.split('/').collect();
 
     let mut attestation_agent = AttestationAgent::new();
 
-    let mut resource_desc_map = HashMap::new();
-    resource_desc_map.insert("repository".to_string(), resource_path[0].to_string());
-    resource_desc_map.insert("type".to_string(), resource_path[1].to_string());
-    resource_desc_map.insert("tag".to_string(), resource_path[2].to_string());
-
-    let resource_desc = ResourceDescription {
-        name: String::default(),
-        optional: resource_desc_map,
-    };
-
     let resource_byte = attestation_agent
-        .download_confidential_resource(
-            CC_KBC_NAME,
-            &cli.kbs_url,
-            &serde_json::to_string(&resource_desc)?,
-        )
+        .download_confidential_resource(CC_KBC_NAME, &cli.resource_uri)
         .await?;
 
     println!("Resource: {:?}", &resource_byte);
