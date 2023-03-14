f8cbf9b47a3964098d8dd56a1d4e2293ba4c0c21
Jiale Zhang <zhangjiale@linux.alibaba.com>
2023-03-14 06:08:52 -0400

    Feat gRPC AS mode
    KBS needs to integrate Attestation-Service, and then KBS can forward TEE evidence to it,
    and verify the evidence according to the owner's configured policy.
    Attestation Service should be integrated by KBS in two forms:
    
    1. As a Rust library crate.
    2. As a remote gRPC server application.
    
    We should provide both above two options.
    The first one can be used by users who want the KBS deployment to be as simple as possible.
    The second type will provide more complete functions.
    Now KBS integrates Attestation-Service in the way of library crate.
    This commit feat the way of gRPC communication, and use them as two optional features.
    
    Works of this commit:
    
    1. Three compilation optional features are set: `grpc-as`, `native-as` and `native-as-no-verifier`. Them are used to control the integration mode of AS.
    2. Set a common trait object to abstract AS to encapsulate different forms of AS integration.
    3. Two optional fields are added in KBS config to specify the address of gRPC AS and the configuration file path of native AS.
    
    Related issue: #18
    Fixed: #44
    
    Signed-off-by: Jiale Zhang <zhangjiale@linux.alibaba.com>

---
+++ src/api_server/src/attest/native.rs
@@ -0,0 +1,41 @@
+// Copyright (c) 2023 by Alibaba.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use super::Attest;
+use anyhow::*;
+use as_types::AttestationResults;
+use async_trait::async_trait;
+use attestation_service::{config::Config as AsConfig, AttestationService};
+use kbs_types::Tee;
+use std::path::Path;
+
+pub struct Native {
+    inner: AttestationService,
+}
+
+#[async_trait]
+impl Attest for Native {
+    async fn attest_verify(
+        &mut self,
+        tee: Tee,
+        nonce: &str,
+        attestation: &str,
+    ) -> Result<AttestationResults> {
+        self.inner.evaluate(tee, nonce, attestation).await
+    }
+}
+
+impl Native {
+    pub fn new(as_config_path: &Option<String>) -> Result<Self> {
+        let as_config = match as_config_path {
+            Some(path) => AsConfig::try_from(Path::new(&path))
+                .map_err(|e| anyhow!("Read AS config file failed: {:?}", e))?,
+            None => AsConfig::default(),
+        };
+
+        Ok(Self {
+            inner: AttestationService::new(as_config)?,
+        })
+    }
+}
