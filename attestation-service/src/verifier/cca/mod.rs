c0b7bd43ba0d4365190d662bb1503542f73ceb8e
Dave Chen <dave.chen@arm.com>
2023-04-10 15:34:26 +0800

    New tee type: CCA (Confidential Compute Architecture)
    CCA verifier is actually done by forwarding the evidence to Verasion[1].
    
    [1] https://github.com/veraison/services
    
    Signed-off-by: Dave Chen <dave.chen@arm.com>

---
+++ attestation-service/src/verifier/cca/mod.rs
@@ -0,0 +1,261 @@
+// Copyright (c) 2023 Arm Ltd.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+use super::*;
+use anyhow::{anyhow, Context, Result};
+use async_trait::async_trait;
+use core::result::Result::Ok;
+use serde::{Deserialize, Serialize};
+use serde_json::{Map, Value};
+use veraison_apiclient::*;
+
+const VERAISON_ADDR: &str = "VERAISON_ADDR";
+const DEFAULT_VERAISON_ADDR: &str = "localhost:8080";
+const MEDIA_TYPE: &str = "application/eat-collection; profile=http://arm.com/CCA-SSD/1.0.0";
+
+#[derive(Debug, Default)]
+pub struct CCA {}
+
+#[derive(Serialize, Deserialize)]
+struct CcaEvidence {
+    /// CCA token
+    token: Vec<u8>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct RealmToken {
+    //cca_realm_personalization_value: String,
+    cca_realm_initial_measurement: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct Evidence {
+    cca_realm_delegated_token: RealmToken,
+}
+
+fn my_evidence_builder(
+    nonce: &[u8],
+    accept: &[String],
+    token: Vec<u8>,
+) -> Result<(Vec<u8>, String), veraison_apiclient::Error> {
+    log::info!("server challenge: {:?}", nonce);
+    log::info!("acceptable media types: {:#?}", accept);
+    // TODO: Get the CCA media type from the slice of `accept`.
+    Ok((token, MEDIA_TYPE.to_string()))
+}
+
+// TODO: compare the nonce with CCA challenge to avoid the replay attack, see: https://github.com/confidential-containers/attestation-service/issues/127
+#[async_trait]
+impl Verifier for CCA {
+    async fn evaluate(
+        &self,
+        _nonce: String,
+        attestation: &Attestation,
+    ) -> Result<TeeEvidenceParsedClaim> {
+        let evidence = serde_json::from_str::<CcaEvidence>(&attestation.tee_evidence)
+            .context("Deserialize CCA Evidence failed.")?;
+
+        let host_url =
+            std::env::var(VERAISON_ADDR).unwrap_or_else(|_| DEFAULT_VERAISON_ADDR.to_string());
+
+        let discovery = Discovery::from_base_url(format!("http://{:}", host_url))?;
+
+        let verification_api = discovery.get_verification_api().await?;
+
+        let relative_endpoint = verification_api
+            .get_api_endpoint("newChallengeResponseSession")
+            .context("Failed to discover the verification endpoint details.")?;
+
+        let api_endpoint = format!("http://{:}{}", host_url, relative_endpoint);
+
+        // create a ChallengeResponse object
+        let cr = ChallengeResponseBuilder::new()
+            .with_new_session_url(api_endpoint)
+            .build()?;
+
+        let nonce = Nonce::Size(32);
+        let token = evidence.token;
+        match cr.run(nonce, my_evidence_builder, token.clone()).await {
+            Err(e) => {
+                log::error!("Error: {}", e);
+                return Err(anyhow!("Attestation failed with error: {:?}", e));
+            }
+            Ok(attestation_result) => {
+                log::info!("Attestation Result: {}", attestation_result)
+            }
+        }
+
+        // NOTE: The tcb returned is actually an empty `Evidence`, the code here is just a show case the parse of the CCA token
+        // to get the tcb is possible, but this is not actually fully implemented due to the below reasons:
+        // 1. CCA validation by the Verasion has some overlapping with the RVPS, the similar validation has been done by the Verasion already.
+        // 2. Each of key of the CCA token layout after the parse is an int from hex, it cannot be converted into a json easily without
+        // manually manipulation, which is dirty and complex, we can hold this for an while and see if the type of key can be redefined as String.
+        let tcb = parse_cca_token(token)?;
+        // Return Evidence parsed claim
+        cca_generate_parsed_claim(tcb).map_err(|e| anyhow!("error from CCA Verifier: {:?}", e))
+    }
+}
+
+/// The expected token layout looks like below,
+///
+/// In short:
+/// {
+/// "cca-platform-token" (44234): {
+///     ...
+///   },
+/// "cca-realm-delegated-token" (44241): {
+///     ...
+///   }
+/// }
+///
+/// and the details for each of them is listed here:
+///
+/// {
+///     265_1: "http://arm.com/CCA-SSD/1.0.0",
+///     10: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
+///     2396_1: h'7f454c4602010100000000000000000003003e000100000050580000000000004000000000000000a0030200000000000000000040003800090040001c001b00',
+///     256_1: h'0107060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
+///     2401_1: h'0107060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
+///     2395_1: 12291_1,
+///     2402_1: "sha-256",
+///     2399_1: [
+///         {
+///             1: "BL",
+///             5: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
+///             4: "3.4.2",
+///             2: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
+///             6: "sha-256",
+///         },
+///         {
+///             1: "M1",
+///             5: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
+///             4: "1.2",
+///             2: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
+///         },
+///         {
+///             1: "M2",
+///             5: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
+///             4: "1.2.3",
+///             2: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
+///         },
+///         {
+///             1: "M3",
+///             5: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
+///             4: "1",
+///             2: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
+///         },
+///     ],
+///     2400_1: "whatever.com",
+/// }
+/// {
+///     10: h'00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+///     44235_1: h'00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+///     44237_1: h'0476f988091be585ed41801aecfab858548c63057e16b0e676120bbd0d2f9c29e056c5d41a0130eb9c21517899dc23146b28e1b062bd3ea4b315fd219f1cbb528cb6e74ca49be16773734f61a1ca61031b2bbf3d918f2f94ffc4228e50919544ae',
+///     44236_1: "sha-256",
+///     44240_1: "sha-256",
+///     44238_1: h'75a1fbc79a7d20a5ff843b914dfd8093d40cd07dd633401c8c42d697be224801',
+///     44239_1: [
+///         h'0000000000000000000000000000000000000000000000000000000000000000',
+///         h'0000000000000000000000000000000000000000000000000000000000000000',
+///         h'0000000000000000000000000000000000000000000000000000000000000000',
+///         h'0000000000000000000000000000000000000000000000000000000000000000',
+///     ],
+/// }
+fn parse_cca_token(token: Vec<u8>) -> Result<Evidence> {
+    let evidence = Evidence {
+        cca_realm_delegated_token: RealmToken {
+            cca_realm_initial_measurement: "".to_string(),
+        },
+    };
+
+    // NOTE: For some corner case, the date cannot be parsed to token correctly but the token
+    // can be validated successfully by the CCA verifier e.g. `Err` value: Todo("Remaining bytes (00)")'
+    // Instead of throwing an error, just print it out in this case.
+    let mut di = match cbor_diag::parse_bytes(token) {
+        Ok(di) => di,
+        Err(err) => {
+            log::info!("Error: {:?}", err);
+            return Ok(evidence);
+        }
+    };
+
+    if let cbor_diag::DataItem::Tag {
+        tag: _,
+        bitwidth: _,
+        value,
+    } = di
+    {
+        di = *value;
+    }
+
+    if let cbor_diag::DataItem::Map { data, .. } = di {
+        for item in data {
+            let cbor_diag::DataItem::ByteString(t) = item.1 else {
+                anyhow::bail!("DateItem is not a ByteString");
+            };
+
+            let val = cbor_diag::parse_bytes(t.data)?;
+
+            let cbor_diag::DataItem::Tag { value, .. } = val else {
+                anyhow::bail!("DateItem is not a Tag");
+            };
+
+            let cbor_diag::DataItem::Array { data, .. } = *value else {
+                anyhow::bail!("DateItem is not a Array");
+            };
+
+            if let cbor_diag::DataItem::ByteString(cose) = data
+                .get(2)
+                .ok_or_else(|| anyhow!("Cannot get raw bytes from token"))?
+            {
+                let v = &cose.data;
+                match cbor_diag::parse_bytes(v) {
+                    Ok(claims) => {
+                        info!("{}", claims.to_diag_pretty());
+                    }
+                    Err(e) => {
+                        error!("Error parsing claims: {}", e);
+                    }
+                }
+            };
+        }
+    }
+
+    Ok(evidence)
+}
+
+fn cca_generate_parsed_claim(tcb: Evidence) -> Result<TeeEvidenceParsedClaim> {
+    let mut claim_map = Map::new();
+
+    claim_map.insert(
+        "cca-realm-initial-measurement".to_string(),
+        serde_json::Value::String(tcb.cca_realm_delegated_token.cca_realm_initial_measurement),
+    );
+
+    log::info!("\nParsed Evidence claims map: \n{:?}\n", &claim_map);
+
+    Ok(Value::Object(claim_map) as TeeEvidenceParsedClaim)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn test_cca_generate_parsed_claim() {
+        let s = fs::read("../test_data/cca-claims.json").unwrap();
+        let evidence = String::from_utf8_lossy(&s);
+        let tcb = serde_json::from_str::<Evidence>(&evidence).unwrap();
+        let parsed_claim = cca_generate_parsed_claim(tcb);
+        assert!(parsed_claim.is_ok());
+        let _ = fs::write(
+            "test_data/cca_evidence_claim_output.txt",
+            format!("{:?}", parsed_claim.unwrap()),
+        );
+    }
+}
