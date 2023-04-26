6c47d08ae3e32cc0e9a8cfa13d0727f7ca700409
Magnus Kulke <magnuskulke@microsoft.com>
2023-04-26 16:45:13 +0200

    Update to az-snp-vtpm 0.2
    * Most validation logic is performed in the crate now
    * VCEK is part of the evidence now, ASK + ARK are bundled in the sev
      crate
    
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>

--- src/verifier/az_snp_vtpm/mod.rs
+++ src/verifier/az_snp_vtpm/mod.rs
@@ -6,13 +6,15 @@
 use super::{Attestation, TeeEvidenceParsedClaim, Verifier};
 use anyhow::{anyhow, Context, Result};
 use async_trait::async_trait;
-use az_snp_vtpm::certs::{get_chain_from_amd, get_vcek_from_amd};
-use az_snp_vtpm::hcl::{buf_to_hcl_data, HclReportWithRuntimeData};
+use az_snp_vtpm::certs::{AmdChain, Vcek, X509};
+use az_snp_vtpm::hcl::{HclData, RuntimeData};
 use az_snp_vtpm::report::Validateable;
 use az_snp_vtpm::vtpm::{Quote, VerifyVTpmQuote};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use sha2::{Digest, Sha256, Sha384};
+use sev::certs::builtin::milan::{ARK, ASK};
+use sev::firmware::guest::types::{AttestationReport, SnpTcbVersion};
+use sha2::{Digest, Sha384};
 use std::collections::BTreeMap;
 
 const HCL_VMPL_VALUE: u32 = 0;
@@ -21,6 +23,7 @@ const HCL_VMPL_VALUE: u32 = 0;
 struct Evidence {
     quote: Quote,
     report: Vec<u8>,
+    vcek: String,
 }
 
 #[derive(Default)]
@@ -36,106 +39,89 @@ impl Verifier for AzSnpVtpm {
         let evidence = serde_json::from_str::<Evidence>(&attestation.tee_evidence)
             .context("Failed to deserialize vTPM SEV-SNP evidence")?;
 
-        let hcl_report: HclReportWithRuntimeData = evidence.report[..].try_into()?;
+        let hcl_data: HclData = evidence.report[..].try_into()?;
+        let snp_report = hcl_data.report().snp_report();
+        let vcek = Vcek::from_pem(&evidence.vcek)?;
+
         let hashed_quote = hash_quote(attestation, &nonce);
 
-        verify_quote(&evidence.quote, &hcl_report, &hashed_quote)?;
-        verify_snp_report(&hcl_report)?;
-        verify_report_data(&hcl_report, &evidence.report)?;
+        verify_quote(&evidence.quote, &hcl_data, &hashed_quote)?;
+        verify_snp_report(snp_report, &vcek)?;
+        let var_data = hcl_data.var_data();
+        hcl_data.report().verify_report_data(var_data)?;
 
-        let claim = parse_tee_evidence(&hcl_report);
+        let claim = parse_tee_evidence(snp_report);
         Ok(claim)
     }
 }
 
-fn verify_report_data(report: &HclReportWithRuntimeData, bytes: &[u8]) -> Result<()> {
-    let (_, var_data) = buf_to_hcl_data(bytes)?;
+fn verify_quote(quote: &Quote, hcl_data: &HclData, hashed_nonce: &[u8]) -> Result<()> {
+    let runtime_data: RuntimeData = hcl_data.var_data().try_into()?;
+    let ak_pub = runtime_data.get_attestation_key()?;
 
-    let mut hasher = Sha256::new();
-    hasher.update(var_data);
-    let var_data_hash = hasher.finalize().to_vec();
-
-    // Only the first 32 bytes of SNP report data are used for the sha256
-    let report_data = &report.snp_report().report_data[..32];
-
-    if var_data_hash != report_data {
-        return Err(anyhow!(
-            "SNP report data field is not matching runtime data"
-        ));
-    }
-
-    Ok(())
-}
-
-fn verify_quote(
-    quote: &Quote,
-    report: &HclReportWithRuntimeData,
-    hashed_nonce: &[u8],
-) -> Result<()> {
-    let ak_pub = report.get_attestation_key()?;
-
-    let result = ak_pub
-        .verify_quote(quote, Some(hashed_nonce))
+    ak_pub
+        .verify_quote(quote, hashed_nonce)
         .context("Failed to verify vTPM quote")?;
 
-    if !result {
-        return Err(anyhow!("vTPM quote is invalid"));
-    }
     Ok(())
 }
 
-fn parse_tee_evidence(hcl_report: &HclReportWithRuntimeData) -> TeeEvidenceParsedClaim {
-    // TODO: copied w/ minor adjustments from a corresponding SEV-SNP pull request:
-    // https://github.com/confidential-containers/attestation-service/pull/38
-    // This logic is shared and should be consolidated into a common place.
+fn verify_snp_report(snp_report: &AttestationReport, vcek: &Vcek) -> Result<()> {
+    let amd_chain = AmdChain {
+        ask: X509::from_der(ASK)?,
+        ark: X509::from_der(ARK)?,
+    };
 
-    let report = hcl_report.snp_report();
-    let numbers = [
-        ("policy_abi_major", report.policy.abi_major()),
-        ("policy_abi_minor", report.policy.abi_minor()),
-        ("policy_smt_allowed", report.policy.smt_allowed()),
-        ("policy_migrate_ma", report.policy.migrate_ma_allowed()),
-        ("policy_debug_allowed", report.policy.debug_allowed()),
-        (
-            "policy_single_socket",
-            report.policy.single_socket_required(),
-        ),
-        // versioning info
-        (
-            "reported_tcb_bootloader",
-            report.reported_tcb.boot_loader as u64,
-        ),
-        ("reported_tcb_tee", report.reported_tcb.tee as u64),
-        ("reported_tcb_snp", report.reported_tcb.snp as u64),
-        (
-            "reported_tcb_microcode",
-            report.reported_tcb.microcode as u64,
-        ),
-        // platform info
-        ("platform_tsme_enabled", report.plat_info.tsme_enabled()),
-        ("platform_smt_enabled", report.plat_info.smt_enabled()),
-    ];
-
-    let mut string_map: BTreeMap<_, _> = numbers.iter().map(|(k, v)| (*k, v.to_string())).collect();
-    string_map.insert("measurement", base64::encode(report.measurement));
-
-    json!(string_map) as TeeEvidenceParsedClaim
-}
-
-fn verify_snp_report(report: &HclReportWithRuntimeData) -> Result<()> {
-    let snp_report = report.snp_report();
-    let vcek = get_vcek_from_amd(snp_report)?;
-    let cert_chain = get_chain_from_amd()?;
-
-    cert_chain.validate()?;
-    vcek.validate(&cert_chain)?;
-    snp_report.validate(&vcek)?;
+    amd_chain
+        .validate()
+        .context("Verification of AMD certificate chain failed")?;
+    vcek.validate(&amd_chain)
+        .context("Verification of VCEK evidence failed")?;
+    snp_report
+        .validate(vcek)
+        .context("Verification of SEV-SNP report failed")?;
 
     if snp_report.vmpl != HCL_VMPL_VALUE {
         return Err(anyhow!("VMPL of SNP report is not {HCL_VMPL_VALUE}"));
     }
 
     Ok(())
+}
+
+fn parse_tee_evidence(report: &AttestationReport) -> TeeEvidenceParsedClaim {
+    let SnpTcbVersion {
+        boot_loader,
+        tee,
+        snp,
+        microcode,
+        ..
+    } = report.reported_tcb;
+    let policy = report.policy;
+
+    let num_values = [
+        ("policy_abi_major", policy.abi_major()),
+        ("policy_abi_minor", policy.abi_minor()),
+        ("policy_smt_allowed", policy.smt_allowed()),
+        ("policy_migrate_ma", policy.migrate_ma_allowed()),
+        ("policy_debug_allowed", policy.debug_allowed()),
+        ("policy_single_socket", policy.single_socket_required()),
+        // versioning info
+        ("reported_tcb_bootloader", boot_loader as u64),
+        ("reported_tcb_tee", tee as u64),
+        ("reported_tcb_snp", snp as u64),
+        ("reported_tcb_microcode", microcode as u64),
+        // platform info
+        ("platform_tsme_enabled", report.plat_info.tsme_enabled()),
+        ("platform_smt_enabled", report.plat_info.smt_enabled()),
+    ];
+
+    let mut string_map: BTreeMap<_, _> = num_values
+        .iter()
+        .map(|(k, v)| (*k, v.to_string()))
+        .collect();
+    string_map.insert("measurement", base64::encode(report.measurement));
+
+    json!(string_map) as TeeEvidenceParsedClaim
 }
 
 fn hash_quote(attestation: &Attestation, nonce: &str) -> Vec<u8> {
