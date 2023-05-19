7110c75b446d254c4d6c6b22365a41e698ccc0cb
Suraj Deshmukh <suraj.deshmukh@microsoft.com>
2023-05-19 15:17:34 -0700

    k8s: Add configuration
    Fixes: #53
    
    Signed-off-by: Suraj Deshmukh <suraj.deshmukh@microsoft.com>

---
+++ config/kubernetes/deploy-kbs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+k8s_cnf_dir="$(dirname ${BASH_SOURCE[0]})"
+
+# Fail the script if the key.bin file does not exist.
+key_file="${k8s_cnf_dir}/overlays/key.bin"
+[[ -f "${key_file}" ]] || {
+    echo "key.bin file does not exist"
+    exit 1
+}
+
+# Create a file kbs.pem if it does not exist.
+kbs_cert="${k8s_cnf_dir}/base/kbs.pem"
+[[ -f "${kbs_cert}" ]] || {
+    openssl genpkey -algorithm ed25519 >"${k8s_cnf_dir}/base/kbs.key"
+    openssl pkey -in "${k8s_cnf_dir}/base/kbs.key" -pubout -out "${kbs_cert}"
+}
+
+kubectl apply -k "./${k8s_cnf_dir}/overlays"
