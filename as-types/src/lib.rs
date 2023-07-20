a07d0fcbd0d309d92e0d14dabdf002f9e806f47e
Jiale Zhang <zhangjiale@linux.alibaba.com>
2023-07-20 11:38:56 +0800

    Change the return value of the evaluate API to token
    Signed-off-by: Jiale Zhang <zhangjiale@linux.alibaba.com>

--- as-types/src/lib.rs
+++ as-types/src/lib.rs
@@ -1,53 +1,6 @@
-use kbs_types::Tee;
 use serde::{Deserialize, Serialize};
 
 pub type TeeEvidenceParsedClaim = serde_json::Value;
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct AttestationResults {
-    tee: Tee,
-    allow: bool,
-    output: ResultOutput,
-    tcb: Option<String>,
-}
-
-impl AttestationResults {
-    pub fn new(
-        tee: Tee,
-        allow: bool,
-        verifier_output: Option<String>,
-        policy_engine_output: Option<String>,
-        tcb: Option<String>,
-    ) -> Self {
-        Self {
-            tee,
-            allow,
-            output: ResultOutput {
-                verifier_output,
-                policy_engine_output,
-            },
-            tcb,
-        }
-    }
-
-    pub fn allow(&self) -> bool {
-        self.allow
-    }
-
-    pub fn tee(&self) -> Tee {
-        self.tee.clone()
-    }
-
-    pub fn output(&self) -> &ResultOutput {
-        &self.output
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct ResultOutput {
-    pub verifier_output: Option<String>,
-    pub policy_engine_output: Option<String>,
-}
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SetPolicyInput {
