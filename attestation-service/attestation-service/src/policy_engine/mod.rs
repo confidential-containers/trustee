acfee5b23963d38438db2446ab9ce4fa59c3f5d5
Xynnn007 <xynnn@linux.alibaba.com>
2023-11-24 16:23:39 +0800

    AS: Change the API of evaluate function of AS
    Before this commit, the API of Attestation-Service lib is coupled with
    KBS protocol.
    
    Also, not all functionalities of verifier driver are used, including
    checking the binding of initdata hash and runtime data hash.
    
    This commit will help to only have attestation related parameters in
    this API.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- attestation-service/attestation-service/src/policy_engine/mod.rs
+++ attestation-service/attestation-service/src/policy_engine/mod.rs
@@ -27,7 +27,6 @@ pub enum PolicyType {
 }
 
 impl PolicyEngineType {
-    #[allow(dead_code)]
     pub fn to_policy_engine(&self, work_dir: &Path) -> Result<Box<dyn PolicyEngine + Send + Sync>> {
         match self {
             PolicyEngineType::OPA => Ok(Box::new(opa::OPA::new(work_dir.to_path_buf())?)
@@ -36,14 +35,22 @@ impl PolicyEngineType {
     }
 }
 
+type PolicyDigest = String;
+type EvaluationResult = serde_json::Value;
+
 #[async_trait]
 pub trait PolicyEngine {
+    /// The result is a key-value map.
+    /// - `key`: the policy id
+    /// - `value`: It will be a tuple. The first element is the digest of
+    /// the policy (using **Sha384**). The second element is the evaluation
+    /// output of the policy.
     async fn evaluate(
         &self,
         reference_data_map: HashMap<String, Vec<String>>,
         input: String,
-        policy_id: Option<String>,
-    ) -> Result<String>;
+        policy_ids: Vec<String>,
+    ) -> Result<HashMap<String, (PolicyDigest, EvaluationResult)>>;
 
     async fn set_policy(&mut self, input: SetPolicyInput) -> Result<()>;
 }
