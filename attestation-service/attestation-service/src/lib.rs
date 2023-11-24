f753fe5bee5eed771f6dc70aebd0b9e2cf412077
Xynnn007 <xynnn@linux.alibaba.com>
2023-11-24 16:23:39 +0800

    AS: Change the API of evaluate function of AS
    Before this commit, the API of Attestation-Service lib is coupled with
    KBS protocol.
    
    Also, not all functionalities of verifier driver are used, including
    checking the binding of initdata hash and runtime data hash.
    
    This commit will help to only have attestation related parameters in
    this API.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- attestation-service/attestation-service/src/lib.rs
+++ attestation-service/attestation-service/src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! # Features
 //! - `rvps-grpc`: The AS will connect a remote RVPS.
-//! - `rvps-native`: The AS will integrate RVPS functionalities itself.
+//! - `rvps-builtin`: The AS will integrate RVPS functionalities itself.
 
 pub mod config;
 pub mod policy_engine;
@@ -18,13 +18,63 @@ pub use kbs_types::{Attestation, Tee};
 use log::debug;
 use policy_engine::{PolicyEngine, PolicyEngineType, SetPolicyInput};
 use rvps::RvpsApi;
-use serde_json::json;
-use sha2::{Digest, Sha384};
+use serde_json::{json, Value};
+use sha2::{Digest, Sha256, Sha384, Sha512};
 use std::{collections::HashMap, str::FromStr};
+use strum::{AsRefStr, EnumString};
 use tokio::fs;
 use verifier::{InitDataHash, ReportData};
 
 use crate::utils::flatten_claims;
+
+/// Hash algorithms used to calculate runtime/init data binding
+#[derive(EnumString, AsRefStr)]
+pub enum HashAlgorithm {
+    #[strum(ascii_case_insensitive)]
+    Sha256,
+
+    #[strum(ascii_case_insensitive)]
+    Sha384,
+
+    #[strum(ascii_case_insensitive)]
+    Sha512,
+}
+
+impl HashAlgorithm {
+    fn accumulate_hash(&self, materials: Vec<u8>) -> Vec<u8> {
+        match self {
+            HashAlgorithm::Sha256 => {
+                let mut hasher = Sha256::new();
+                hasher.update(materials);
+                hasher.finalize().to_vec()
+            }
+            HashAlgorithm::Sha384 => {
+                let mut hasher = Sha384::new();
+                hasher.update(materials);
+                hasher.finalize().to_vec()
+            }
+            HashAlgorithm::Sha512 => {
+                let mut hasher = Sha512::new();
+                hasher.update(materials);
+                hasher.finalize().to_vec()
+            }
+        }
+    }
+}
+
+/// Runtime/Init Data used to check the binding relationship with report data
+/// in Evidence
+pub enum Data {
+    /// This will be used as the expected runtime/init data to check against
+    /// the one inside evidence.
+    Raw(Vec<u8>),
+
+    /// Runtime/Init data in a JSON map. CoCoAS will rearrange each layer of the
+    /// data JSON object in dictionary order by key, then serialize and output
+    /// it into a compact string, and perform hash calculation on the whole
+    /// to check against the one inside evidence.
+    Structured(Value),
+}
 
 pub struct AttestationService {
     _config: Config,
@@ -72,44 +122,53 @@ impl AttestationService {
             .map_err(|e| anyhow!("Cannot Set Policy: {:?}", e))
     }
 
-    fn accumulate_hash(materials: &[Vec<u8>]) -> Option<Vec<u8>> {
-        if materials.is_empty() {
-            return None;
-        }
-        let mut hasher = Sha384::new();
-        materials.iter().for_each(|m| hasher.update(m));
-        Some(hasher.finalize().to_vec())
-    }
-
     /// Evaluate Attestation Evidence.
-    /// Issue an attestation results token which contain TCB status and TEE public key.
-    pub async fn evaluate(&self, tee: Tee, nonce: &str, attestation: &str) -> Result<String> {
-        let attestation = serde_json::from_str::<Attestation>(attestation)
-            .context("Failed to deserialize Attestation")?;
+    /// Issue an attestation results token which contain TCB status and TEE public key. Input parameters:
+    /// - `evidence`: TEE evidence bytes. This might not be the raw hardware evidence bytes. Definitions
+    /// are in `verifier` crate.
+    /// - `tee`: concrete TEE type
+    /// - `runtime_data`: These data field will be used to check against the counterpart inside the evidence.
+    /// The concrete way of checking is decide by the enum type. If this parameter is set `None`, the comparation
+    /// will not be performed.
+    /// - `init_data`: These data field will be used to check against the counterpart inside the evidence.
+    /// The concrete way of checking is decide by the enum type. If this parameter is set `None`, the comparation
+    /// will not be performed.
+    /// - `hash_algorithm`: The hash algorithm that is used to calculate the digest of `runtime_data` and
+    /// `init_data`.
+    /// - `policy_ids`: The policy ids that used to check this evidence. Any check fails against a policy will
+    /// not cause this function to return error. The result check against every policy will be included inside
+    /// the finally Token returned by CoCo-AS.
+    #[allow(clippy::too_many_arguments)]
+    pub async fn evaluate(
+        &self,
+        evidence: Vec<u8>,
+        tee: Tee,
+        runtime_data: Option<Data>,
+        runtime_data_hash_algorithm: HashAlgorithm,
+        init_data: Option<Data>,
+        init_data_hash_algorithm: HashAlgorithm,
+        policy_ids: Vec<String>,
+    ) -> Result<String> {
         let verifier = verifier::to_verifier(&tee)?;
 
-        let report_data = Self::accumulate_hash(&[
-            nonce.as_bytes().to_vec(),
-            attestation.tee_pubkey.k_mod.as_bytes().to_vec(),
-            attestation.tee_pubkey.k_exp.as_bytes().to_vec(),
-        ]);
+        let (report_data, runtime_data_claims) =
+            parse_data(runtime_data, &runtime_data_hash_algorithm).context("parse runtime data")?;
 
         let report_data = match &report_data {
-            Some(value) => ReportData::Value(value),
+            Some(data) => ReportData::Value(data),
             None => ReportData::NotProvided,
         };
 
+        let (init_data, init_data_claims) =
+            parse_data(init_data, &init_data_hash_algorithm).context("parse init data")?;
+
+        let init_data_hash = match &init_data {
+            Some(data) => InitDataHash::Value(data),
+            None => InitDataHash::NotProvided,
+        };
+
         let claims_from_tee_evidence = verifier
-            .evaluate(
-                attestation.tee_evidence.as_bytes(),
-                &report_data,
-                // We currently do not need to check the initdata hash in AS when using
-                // `verifier` crate.
-                //
-                // We will refactor the CoCo-AS' API to leverage the parameter.
-                // See https://github.com/confidential-containers/kbs/issues/177
-                &InitDataHash::NotProvided,
-            )
+            .evaluate(&evidence, &report_data, &init_data_hash)
             .await
             .map_err(|e| anyhow!("Verifier evaluate failed: {e:?}"))?;
 
@@ -122,18 +181,38 @@ impl AttestationService {
             .await
             .map_err(|e| anyhow!("Generate reference data failed{:?}", e))?;
 
-        // Now only support using default policy to evaluate
         let evaluation_report = self
             .policy_engine
-            .evaluate(reference_data_map, tcb_json, None)
+            .evaluate(reference_data_map.clone(), tcb_json, policy_ids.clone())
             .await
             .map_err(|e| anyhow!("Policy Engine evaluation failed: {e}"))?;
 
+        let policies: Vec<_> = evaluation_report
+            .into_iter()
+            .map(|(k, v)| {
+                json!({
+                    "policy-id": k,
+                    "policy-hash": v.0,
+                    "evaluation-result": v.1,
+                })
+            })
+            .collect();
+
+        let reference_data_map: HashMap<String, Vec<String>> = reference_data_map
+            .into_iter()
+            .filter(|it| !it.1.is_empty())
+            .collect();
+
         let token_claims = json!({
-            "tee-pubkey": attestation.tee_pubkey.clone(),
+            "evaluation-reports": policies,
             "tcb-status": flattened_claims,
-            "evaluation-report": evaluation_report,
+            "reference-data": reference_data_map,
+            "customized_claims": {
+                "init_data": init_data_claims,
+                "runtime_data": runtime_data_claims,
+            },
         });
+
         let attestation_results_token = self.token_broker.issue(token_claims)?;
 
         Ok(attestation_results_token)
@@ -157,5 +236,50 @@ impl AttestationService {
     /// Registry a new reference value
     pub async fn register_reference_value(&mut self, message: &str) -> Result<()> {
         self.rvps.verify_and_extract(message).await
+    }
+}
+
+/// Get the expected init/runtime data and potential claims due to the given input
+/// and the hash algorithm
+fn parse_data(
+    data: Option<Data>,
+    hash_algorithm: &HashAlgorithm,
+) -> Result<(Option<Vec<u8>>, Value)> {
+    match data {
+        Some(value) => match value {
+            Data::Raw(raw) => Ok((Some(raw), Value::Null)),
+            Data::Structured(structured) => {
+                // by default serde_json will enforence the alphabet order for keys
+                let hash_materials =
+                    serde_json::to_vec(&structured).context("parse JSON structured data")?;
+                let digest = hash_algorithm.accumulate_hash(hash_materials);
+                Ok((Some(digest), structured))
+            }
+        },
+        None => Ok((None, Value::Null)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_json_diff::assert_json_eq;
+    use rstest::rstest;
+    use serde_json::{json, Value};
+
+    use crate::{Data, HashAlgorithm};
+
+    #[rstest]
+    #[case(Some(Data::Raw(b"aaaaa".to_vec())), Some(b"aaaaa".to_vec()), HashAlgorithm::Sha384, Value::Null)]
+    #[case(None, None, HashAlgorithm::Sha384, Value::Null)]
+    #[case(Some(Data::Structured(json!({"b": 1, "a": "test", "c": {"d": "e"}}))), Some(hex::decode(b"e71ce8e70d814ba6639c3612ebee0ff1f76f650f8dbb5e47157e0f3f525cd22c4597480a186427c813ca941da78870c3").unwrap()), HashAlgorithm::Sha384, json!({"b": 1, "a": "test", "c": {"d": "e"}}))]
+    fn parse_data_json_binding(
+        #[case] input: Option<Data>,
+        #[case] expected_data: Option<Vec<u8>>,
+        #[case] hash_algorithm: HashAlgorithm,
+        #[case] expected_claims: Value,
+    ) {
+        let (data, data_claims) = crate::parse_data(input, &hash_algorithm).expect("parse failed");
+        assert_eq!(data, expected_data);
+        assert_json_eq!(data_claims, expected_claims);
     }
 }
--- attestation-service/attestation-service/src/policy_engine/opa/mod.rs
+++ attestation-service/attestation-service/src/policy_engine/opa/mod.rs
@@ -1,5 +1,5 @@
 use crate::policy_engine::{PolicyEngine, PolicyType};
-use anyhow::{anyhow, bail, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use async_trait::async_trait;
 use base64::Engine;
 use log::debug;
@@ -11,7 +11,7 @@ use std::os::raw::c_char;
 use std::path::PathBuf;
 use std::str::FromStr;
 
-use super::SetPolicyInput;
+use super::{EvaluationResult, PolicyDigest, SetPolicyInput};
 
 // Link import cgo function
 #[link(name = "cgo")]
@@ -63,51 +63,76 @@ impl PolicyEngine for OPA {
         &self,
         reference_data_map: HashMap<String, Vec<String>>,
         input: String,
-        policy_id: Option<String>,
-    ) -> Result<String> {
-        let policy_file_path = format!(
-            "{}/{}.rego",
-            self.policy_dir_path
-                .to_str()
-                .ok_or_else(|| anyhow!("Miss Policy DirPath"))?,
-            policy_id.unwrap_or("default".to_string())
-        );
-        let policy = tokio::fs::read_to_string(policy_file_path)
-            .await
-            .map_err(|e| anyhow!("Read OPA policy file failed: {:?}", e))?;
+        policy_ids: Vec<String>,
+    ) -> Result<HashMap<String, (PolicyDigest, EvaluationResult)>> {
+        let mut res = HashMap::new();
 
-        let policy_go = GoString {
-            p: policy.as_ptr() as *const c_char,
-            n: policy.len() as isize,
-        };
+        let policy_dir_path = self
+            .policy_dir_path
+            .to_str()
+            .ok_or_else(|| anyhow!("Miss Policy DirPath"))?;
 
-        let reference = serde_json::json!({ "reference": reference_data_map }).to_string();
+        for policy_id in policy_ids {
+            let policy_file_path = format!("{policy_dir_path}/{policy_id}.rego");
 
-        let reference_go = GoString {
-            p: reference.as_ptr() as *const c_char,
-            n: reference.len() as isize,
-        };
+            let policy = tokio::fs::read_to_string(policy_file_path)
+                .await
+                .map_err(|e| anyhow!("Read OPA policy file failed: {:?}", e))?;
 
-        let input_go = GoString {
-            p: input.as_ptr() as *const c_char,
-            n: input.len() as isize,
-        };
+            let policy_hash = {
+                use sha2::Digest;
+                let mut hasher = sha2::Sha384::new();
+                hasher.update(&policy);
+                let hex = hasher.finalize().to_vec();
+                hex::encode(hex)
+            };
 
-        // Call the function exported by cgo and process the returned decision
-        let decision_buf: *mut c_char = unsafe { evaluateGo(policy_go, reference_go, input_go) };
-        let decision_str: &CStr = unsafe { CStr::from_ptr(decision_buf) };
-        let res = decision_str.to_str()?.to_string();
-        debug!("Evaluated: {}", res);
-        if res.starts_with("Error::") {
-            return Err(anyhow!(res));
-        }
+            let policy_go = GoString {
+                p: policy.as_ptr() as *const c_char,
+                n: policy.len() as isize,
+            };
 
-        // If a clear approval opinion is given in the evaluation report,
-        // the rejection information will be reflected in the evaluation failure return value.
-        let res_kv: Value = serde_json::from_str(&res)?;
-        if let Some(allow) = res_kv["allow"].as_bool() {
+            let reference = serde_json::json!({ "reference": reference_data_map }).to_string();
+
+            let reference_go = GoString {
+                p: reference.as_ptr() as *const c_char,
+                n: reference.len() as isize,
+            };
+
+            let input_go = GoString {
+                p: input.as_ptr() as *const c_char,
+                n: input.len() as isize,
+            };
+
+            // Call the function exported by cgo and process the returned decision
+            let decision_buf: *mut c_char =
+                unsafe { evaluateGo(policy_go, reference_go, input_go) };
+            let decision_str: &CStr = unsafe { CStr::from_ptr(decision_buf) };
+            let policy_res = decision_str.to_str()?.to_string();
+            debug!("Evaluated: {}", policy_res);
+            if policy_res.starts_with("Error::") {
+                bail!("OPA verification failed: {policy_res}");
+            }
+
+            // If a clear approval opinion is given in the evaluation report,
+            // the rejection information will be reflected in the evaluation failure return value.
+            let res_kv: Value = serde_json::from_str(&policy_res)?;
+
+            // only if there is a field named `allow` in the evaluation report and
+            // it is false, the evaluation fails. Otherwise the evaluation will be
+            // treated as succees.
+            let allow = res_kv
+                .get("allow")
+                .map(|a| a.as_bool())
+                .map(|a| a.unwrap_or(true))
+                .unwrap_or(true);
+
             if !allow {
-                bail!("Untrusted TEE evidence")
+                bail!("TEE evidence does not pass policy {policy_id}, reason: {policy_res}");
+            } else {
+                let evaluation_result =
+                    serde_json::from_str(&policy_res).context("serialize OPA result")?;
+                res.insert(policy_id.to_owned(), (policy_hash, evaluation_result));
             }
         }
 
@@ -141,6 +166,7 @@ impl PolicyEngine for OPA {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use assert_json_diff::assert_json_eq;
     use serde_json::json;
 
     fn dummy_reference(ver: u64) -> String {
@@ -173,15 +199,20 @@ mod tests {
             .evaluate(
                 reference_data.clone(),
                 dummy_input(5, 5),
-                Some(default_policy_id.clone()),
+                vec![default_policy_id.clone()],
             )
             .await;
-        assert!(res.is_ok(), "OPA execution() should be success");
+        let res = res.expect("OPA execution should succeed");
+        // this expected value is calculated by `sha384sum`
+        let expected_digest = "c0e7929671fb6780387f54760d84d65d2ce96093dfb33efda21f5eb05afcda77bba444c02cd177b23a5d350716726157";
+        assert_eq!(expected_digest, res["default_policy"].0);
+        assert_json_eq!(json!({"allow":true}), res["default_policy"].1);
 
         let res = opa
-            .evaluate(reference_data, dummy_input(0, 0), Some(default_policy_id))
+            .evaluate(reference_data, dummy_input(0, 0), vec![default_policy_id])
             .await;
-        assert!(res.is_err(), "OPA execution() should be failed");
+
+        res.expect_err("OPA execution should fail");
     }
 
     #[tokio::test]
