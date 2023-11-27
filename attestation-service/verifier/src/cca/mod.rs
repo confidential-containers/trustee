f6693d84da33a4acad671f39a6c592ce181ddbb9
Dave Chen <dave.chen@arm.com>
2023-11-27 16:13:35 +0800

    CCA: Get the evidence from EAR (EAT Attesation Result)
    CCA validation by the Verasion has some overlapping with the RVPS,
    the similar validation has been done by the Verasion already.
    So the validation of evidence in CoCo is not needed in theory.
    
    The parse of the CCA token here is to align with other verifier,
    e.g. TDX, and to support RVPS if that is the case of future planning.
    
    NOTE that each of the value we get from EAR is base64 encoded hex value.
    So, the reference value that are registered for RVPS are expected to
    be base64 encoded hex value as well.
    
    [1] https://datatracker.ietf.org/doc/draft-ietf-rats-ar4si/
    
    Signed-off-by: Dave Chen <dave.chen@arm.com>

--- attestation-service/verifier/src/cca/mod.rs
+++ attestation-service/verifier/src/cca/mod.rs
@@ -8,12 +8,11 @@ use anyhow::{anyhow, Context, Result};
 use async_trait::async_trait;
 use base64::Engine;
 use core::result::Result::Ok;
-use ear::Ear;
+use ear::{Ear, RawValue};
 use jsonwebtoken::{self as jwt};
 use log::{error, info, warn};
 use serde::{Deserialize, Serialize};
-use serde_json::{Map, Value};
-use std::str;
+use std::{collections::BTreeMap, str};
 use veraison_apiclient::*;
 
 const VERAISON_ADDR: &str = "VERAISON_ADDR";
@@ -23,23 +22,42 @@ const MEDIA_TYPE: &str = "application/eat-collection; profile=http://arm.com/CCA
 #[derive(Debug, Default)]
 pub struct CCA {}
 
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct SwComponent {
+    pub measurement_type: String,
+    pub measurement_value: String,
+    pub version: String,
+    pub signer_id: String,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct CcaPlatformClaims {
+    pub cca_platform_challenge: String,
+    pub cca_platform_sw_components: Vec<SwComponent>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct RealmClaims {
+    pub cca_realm_personalization_value: String,
+    pub cca_realm_initial_measurement: String,
+    pub cca_realm_extensible_measurements: Vec<String>,
+    pub cca_realm_hash_algo_id: String,
+    pub cca_realm_public_key_hash_algo_id: String,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Evidence {
+    realm: RealmClaims,
+    platform: CcaPlatformClaims,
+}
+
 #[derive(Serialize, Deserialize)]
 struct CcaEvidence {
     /// CCA token
     token: Vec<u8>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "kebab-case")]
-struct RealmToken {
-    //cca_realm_personalization_value: String,
-    cca_realm_initial_measurement: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "kebab-case")]
-struct Evidence {
-    cca_realm_delegated_token: RealmToken,
 }
 
 fn my_evidence_builder(
@@ -47,8 +65,8 @@ fn my_evidence_builder(
     accept: &[String],
     token: Vec<u8>,
 ) -> Result<(Vec<u8>, String), veraison_apiclient::Error> {
-    log::info!("server challenge: {:?}", nonce);
-    log::info!("acceptable media types: {:#?}", accept);
+    info!("server challenge: {:?}", nonce);
+    info!("acceptable media types: {:#?}", accept);
     // TODO: Get the CCA media type from the slice of `accept`.
     Ok((token, MEDIA_TYPE.to_string()))
 }
@@ -104,7 +122,7 @@ impl Verifier for CCA {
         let n = Nonce::Value(expected_report_data.clone());
         let result = match cr.run(n, my_evidence_builder, token.clone()).await {
             Err(e) => {
-                log::error!("Error: {}", e);
+                error!("Error: {}", e);
                 bail!("CCA Attestation failed with error: {:?}", e);
             }
             Ok(attestation_result) => attestation_result,
@@ -118,7 +136,7 @@ impl Verifier for CCA {
             .context("decrypt the ear with the decoding key")?;
 
         let ear_nonce = plain_ear.nonce.context("get nonce from ear")?;
-        let nonce_byte = base64::engine::general_purpose::URL_SAFE
+        let nonce_byte = base64::engine::general_purpose::STANDARD
             .decode(ear_nonce.to_string())
             .context("decode nonce byte from ear")?;
 
@@ -126,29 +144,32 @@ impl Verifier for CCA {
             bail!("report data is different from that in ear's session nonce");
         }
 
+        let cca_mod = match plain_ear.submods.get("CCA_SSD_PLATFORM") {
+            Some(value) => value,
+            None => bail!("no entry found for CCA_SSD_PLATFORM"),
+        };
+        let evidence = &cca_mod.annotated_evidence;
+
         if let InitDataHash::Value(_) = expected_init_data_hash {
             warn!("CCA currently does not support parse `cca_realm_personalization_value`. Init data hash check skipped.");
         }
 
-        // NOTE: The tcb returned is actually an empty `Evidence`, the code here is just a show case the parse of the CCA token
-        // to get the tcb is possible, but this is not actually fully implemented due to the below reasons:
-        // 1. CCA validation by the Verasion has some overlapping with the RVPS, the similar validation has been done by the Verasion already.
-        // 2. Each of key of the CCA token layout after the parse is an int from hex, it cannot be converted into a json easily without
-        // manually manipulation, which is dirty and complex, we can hold this for an while and see if the type of key can be redefined as String.
-        let tcb = parse_cca_token(token)?;
+        // NOTE: CCA validation by the Verasion has some overlapping with the RVPS, the similar validation has been done by the Verasion already.
+        // The generation of CCA evidence here is to align with other verifier, e.g. TDX, and to support RVPS if that is the case of future planning.
+        let tcb = parse_cca_evidence(evidence)?;
         // Return Evidence parsed claim
         cca_generate_parsed_claim(tcb).map_err(|e| anyhow!("error from CCA Verifier: {:?}", e))
     }
 }
 
-/// The expected token layout looks like below,
+/// The expected evidence layout looks like below,
 ///
 /// In short:
 /// {
-/// "cca-platform-token" (44234): {
+/// "platform": {
 ///     ...
 ///   },
-/// "cca-realm-delegated-token" (44241): {
+/// "realm": {
 ///     ...
 ///   }
 /// }
@@ -156,130 +177,81 @@ impl Verifier for CCA {
 /// and the details for each of them is listed here:
 ///
 /// {
-///     265_1: "http://arm.com/CCA-SSD/1.0.0",
-///     10: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
-///     2396_1: h'7f454c4602010100000000000000000003003e000100000050580000000000004000000000000000a0030200000000000000000040003800090040001c001b00',
-///     256_1: h'0107060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
-///     2401_1: h'0107060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
-///     2395_1: 12291_1,
-///     2402_1: "sha-256",
-///     2399_1: [
-///         {
-///             1: "BL",
-///             5: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
-///             4: "3.4.2",
-///             2: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
-///             6: "sha-256",
-///         },
-///         {
-///             1: "M1",
-///             5: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
-///             4: "1.2",
-///             2: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
-///         },
-///         {
-///             1: "M2",
-///             5: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
-///             4: "1.2.3",
-///             2: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
-///         },
-///         {
-///             1: "M3",
-///             5: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
-///             4: "1",
-///             2: h'07060504030201000f0e0d0c0b0a090817161514131211101f1e1d1c1b1a1918',
-///         },
-///     ],
-///     2400_1: "whatever.com",
+///    "platform":{
+///       "cca-platform-challenge":"tZc8touqn8VVWHhrfsZ/aeQN9bpaqSHNDCf0BYegEeo=",
+///       "cca-platform-config":"AQcGBQQDAgEADw4NDAsKCQgXFhUUExIREB8eHRwbGhkY",
+///       "cca-platform-hash-algo-id":"sha-256",
+///       "cca-platform-implementation-id":"f0VMRgIBAQAAAAAAAAAAAAMAPgABAAAAUFgAAAAAAAA=",
+///       "cca-platform-instance-id":"AQcGBQQDAgEADw4NDAsKCQgXFhUUExIREB8eHRwbGhkY",
+///       "cca-platform-lifecycle":12291,
+///       "cca-platform-profile":"http://arm.com/CCA-SSD/1.0.0",
+///       "cca-platform-service-indicator":"whatever.com",
+///       "cca-platform-sw-components":[
+///          {
+///             "measurement-description":"TF-M_SHA256MemPreXIP",
+///             "measurement-type":"BL",
+///             "measurement-value":"BwYFBAMCAQAPDg0MCwoJCBcWFRQTEhEQHx4dHBsaGRg=",
+///             "signer-id":"BwYFBAMCAQAPDg0MCwoJCBcWFRQTEhEQHx4dHBsaGRg=",
+///             "version":"3.4.2"
+///          },
+///          {
+///             "measurement-type":"M1",
+///             "measurement-value":"BwYFBAMCAQAPDg0MCwoJCBcWFRQTEhEQHx4dHBsaGRg=",
+///             "signer-id":"BwYFBAMCAQAPDg0MCwoJCBcWFRQTEhEQHx4dHBsaGRg=",
+///             "version":"1.2"
+///          },
+///          {
+///             "measurement-type":"M2",
+///             "measurement-value":"BwYFBAMCAQAPDg0MCwoJCBcWFRQTEhEQHx4dHBsaGRg=",
+///             "signer-id":"BwYFBAMCAQAPDg0MCwoJCBcWFRQTEhEQHx4dHBsaGRg=",
+///             "version":"1.2.3"
+///          },
+///          {
+///             "measurement-type":"M3",
+///             "measurement-value":"BwYFBAMCAQAPDg0MCwoJCBcWFRQTEhEQHx4dHBsaGRg=",
+///             "signer-id":"BwYFBAMCAQAPDg0MCwoJCBcWFRQTEhEQHx4dHBsaGRg=",
+///             "version":"1"
+///          }
+///       ]
+///    },
+///    "realm":{
+///       "cca-realm-challenge":"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",
+///       "cca-realm-extensible-measurements":[
+///          "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+///          "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+///          "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=",
+///          "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
+///       ],
+///       "cca-realm-hash-algo-id":"sha-256",
+///       "cca-realm-initial-measurement":"EJHTwpx6vz58Z4/NjKCnmOse6cirEeEbPq06H/xIXUw=",
+///       "cca-realm-personalization-value":"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",
+///       "cca-realm-public-key":"BHb5iAkb5YXtQYAa7Pq4WFSMYwV+FrDmdhILvQ0vnCngVsXUGgEw65whUXiZ3CMUayjhsGK9PqSzFf0hnxy7Uoy250ykm+Fnc3NPYaHKYQMbK789kY8vlP/EIo5QkZVErg==",
+///       "cca-realm-public-key-hash-algo-id":"sha-256"
+///    }
 /// }
-/// {
-///     10: h'00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
-///     44235_1: h'00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
-///     44237_1: h'0476f988091be585ed41801aecfab858548c63057e16b0e676120bbd0d2f9c29e056c5d41a0130eb9c21517899dc23146b28e1b062bd3ea4b315fd219f1cbb528cb6e74ca49be16773734f61a1ca61031b2bbf3d918f2f94ffc4228e50919544ae',
-///     44236_1: "sha-256",
-///     44240_1: "sha-256",
-///     44238_1: h'75a1fbc79a7d20a5ff843b914dfd8093d40cd07dd633401c8c42d697be224801',
-///     44239_1: [
-///         h'0000000000000000000000000000000000000000000000000000000000000000',
-///         h'0000000000000000000000000000000000000000000000000000000000000000',
-///         h'0000000000000000000000000000000000000000000000000000000000000000',
-///         h'0000000000000000000000000000000000000000000000000000000000000000',
-///     ],
-/// }
-fn parse_cca_token(token: Vec<u8>) -> Result<Evidence> {
-    let evidence = Evidence {
-        cca_realm_delegated_token: RealmToken {
-            cca_realm_initial_measurement: "".to_string(),
-        },
-    };
+/// NOTE: each of the value are base64 encoded hex value.
+fn parse_cca_evidence(evidence_map: &BTreeMap<String, RawValue>) -> Result<Evidence> {
+    let mut evidence = Evidence::default();
+    let platfrom = evidence_map
+        .get("platform")
+        .context("get platform evidence from the cca evidence map")?;
+    let output = serde_json::to_string(platfrom)?;
+    let p: CcaPlatformClaims = serde_json::from_str(output.as_str())?;
+    evidence.platform = p;
 
-    // NOTE: For some corner case, the date cannot be parsed to token correctly but the token
-    // can be validated successfully by the CCA verifier e.g. `Err` value: Todo("Remaining bytes (00)")'
-    // Instead of throwing an error, just print it out in this case.
-    let mut di = match cbor_diag::parse_bytes(token) {
-        Ok(di) => di,
-        Err(err) => {
-            log::info!("Error: {:?}", err);
-            return Ok(evidence);
-        }
-    };
-
-    if let cbor_diag::DataItem::Tag {
-        tag: _,
-        bitwidth: _,
-        value,
-    } = di
-    {
-        di = *value;
-    }
-
-    if let cbor_diag::DataItem::Map { data, .. } = di {
-        for item in data {
-            let cbor_diag::DataItem::ByteString(t) = item.1 else {
-                anyhow::bail!("DateItem is not a ByteString");
-            };
-
-            let val = cbor_diag::parse_bytes(t.data)?;
-
-            let cbor_diag::DataItem::Tag { value, .. } = val else {
-                anyhow::bail!("DateItem is not a Tag");
-            };
-
-            let cbor_diag::DataItem::Array { data, .. } = *value else {
-                anyhow::bail!("DateItem is not a Array");
-            };
-
-            if let cbor_diag::DataItem::ByteString(cose) = data
-                .get(2)
-                .ok_or_else(|| anyhow!("Cannot get raw bytes from token"))?
-            {
-                let v = &cose.data;
-                match cbor_diag::parse_bytes(v) {
-                    Ok(claims) => {
-                        info!("{}", claims.to_diag_pretty());
-                    }
-                    Err(e) => {
-                        error!("Error parsing claims: {}", e);
-                    }
-                }
-            };
-        }
-    }
+    let realm = evidence_map
+        .get("realm")
+        .context("get realm evidence from the cca evidence map")?;
+    let output = serde_json::to_string(realm)?;
+    let r: RealmClaims = serde_json::from_str(output.as_str())?;
+    evidence.realm = r;
 
     Ok(evidence)
 }
 
 fn cca_generate_parsed_claim(tcb: Evidence) -> Result<TeeEvidenceParsedClaim> {
-    let mut claim_map = Map::new();
-
-    claim_map.insert(
-        "cca-realm-initial-measurement".to_string(),
-        serde_json::Value::String(tcb.cca_realm_delegated_token.cca_realm_initial_measurement),
-    );
-
-    log::info!("\nParsed Evidence claims map: \n{:?}\n", &claim_map);
-
-    Ok(Value::Object(claim_map) as TeeEvidenceParsedClaim)
+    let v = serde_json::to_value(tcb).context("build json value from the cca evidence")?;
+    Ok(v as TeeEvidenceParsedClaim)
 }
 
 #[cfg(test)]
@@ -289,7 +261,7 @@ mod tests {
 
     #[test]
     fn test_cca_generate_parsed_claim() {
-        let s = fs::read("./test_data/cca-claims.json").unwrap();
+        let s = fs::read("../test_data/cca-claims.json").unwrap();
         let evidence = String::from_utf8_lossy(&s);
         let tcb = serde_json::from_str::<Evidence>(&evidence).unwrap();
         let parsed_claim = cca_generate_parsed_claim(tcb);
