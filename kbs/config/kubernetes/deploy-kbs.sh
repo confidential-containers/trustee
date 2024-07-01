2aae7ab86dc732efd08a321df2f6c84bd093e7f0
Hyounggyu Choi <Hyounggyu.Choi@ibm.com>
2024-07-01 14:20:16 +0200

    KBS: Enable deployment for s390x
    The following changes enable KBS deployment with a different configuration for s390x:
    
    - Environment variable declaration: SE_SKIP_CERTS_VERIFICATION
    - Persist volume/volume claim: required attestation credentials
    
    This commit differentiates the {overlays, nodeport} configuration for KBS deployment
    between x86_64 and s390x. It also includes updates to `deploy-kbs.sh`.
    
    Signed-off-by: Hyounggyu Choi <Hyounggyu.Choi@ibm.com>

--- kbs/config/kubernetes/deploy-kbs.sh
+++ kbs/config/kubernetes/deploy-kbs.sh
@@ -6,11 +6,12 @@ set -euo pipefail
 DEPLOYMENT_DIR="${DEPLOYMENT_DIR:-overlays}"
 
 k8s_cnf_dir="$(dirname ${BASH_SOURCE[0]})"
+ARCH=$(uname -m)
 
 # Fail the script if the key.bin file does not exist.
-key_file="${k8s_cnf_dir}/overlays/key.bin"
+key_file="${k8s_cnf_dir}/overlays/${ARCH}/key.bin"
 [[ -f "${key_file}" ]] || {
-    echo "key.bin file does not exist"
+    echo "key.bin not found at ${k8s_cnf_dir}/overlays/${ARCH}/"
     exit 1
 }
 
@@ -21,4 +22,18 @@ kbs_cert="${k8s_cnf_dir}/base/kbs.pem"
     openssl pkey -in "${k8s_cnf_dir}/base/kbs.key" -pubout -out "${kbs_cert}"
 }
 
-kubectl apply -k "./${k8s_cnf_dir}/${DEPLOYMENT_DIR}"
+if [ "${ARCH}" == "s390x" ]; then
+    if [ -n "${IBM_SE_CREDS_DIR:-}" ]; then
+    export NODE_NAME=$(kubectl get nodes -o jsonpath='{.items[0].metadata.name}')
+    envsubst <"${k8s_cnf_dir}/overlays/s390x/pv.yaml" | kubectl apply -f -
+    else
+        echo "IBM_SE_CREDS_DIR is empty" >&2
+        exit 1
+    fi
+fi
+
+if [[ "${DEPLOYMENT_DIR}" == "nodeport" || "${DEPLOYMENT_DIR}" == "overlays" ]]; then
+    kubectl apply -k "${k8s_cnf_dir}/${DEPLOYMENT_DIR}/${ARCH}"
+else
+    kubectl apply -k "${k8s_cnf_dir}/${DEPLOYMENT_DIR}"
+fi
