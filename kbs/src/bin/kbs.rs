45c710a6d853805e6670f782ddeefb2228b17815
Mikko Ylinen <mikko.ylinen@intel.com>
2024-08-30 11:31:42 +0300

    ita: use AttestationTokenVerifier
    Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

--- kbs/src/bin/kbs.rs
+++ kbs/src/bin/kbs.rs
@@ -47,7 +47,7 @@ async fn main() -> Result<()> {
             } else if #[cfg(feature = "coco-as-grpc")] {
                 AttestationService::new(kbs_config.grpc_config.unwrap_or_default()).await?
             } else if #[cfg(feature = "intel-trust-authority-as")] {
-                AttestationService::new(kbs_config.intel_trust_authority_config)?
+                AttestationService::new(kbs_config.intel_trust_authority_config).await?
             } else {
                 compile_error!("Please enable at least one of the following features: `coco-as-builtin`, `coco-as-builtin-no-verifier`, `coco-as-grpc` or `intel-trust-authority-as` to continue.");
             }
