065e115c18121f32eb9bfc3ba9082d44cd4918fa
Xynnn007 <xynnn@linux.alibaba.com>
2024-09-27 15:01:26 +0800

    KBS: add Plugins module
    The Plugins module could provide a plugin way for developers to extend
    the ability of KBS client APIs. This also provides a Sample
    implementation for example.
    
    The oroginal resource is refactored into plugins as a plugin.
    
    Also, we change both `read_secret_resource` and `write_secret_resource`
    to Fn rather than FnMut. This leaves the synchronization handling to
    concrete underlying plugins, thus promote the performance because we
    can avoid a global Mutex.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

---
+++ kbs/src/plugins/implementations/mod.rs
@@ -0,0 +1,9 @@
+// Copyright (c) 2024 by Alibaba.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod resource;
+pub mod sample;
+
+pub use resource::{RepositoryConfig, ResourceStorage};
+pub use sample::{Sample, SampleConfig};
---
+++ kbs/src/plugins/implementations/sample.rs
@@ -0,0 +1,64 @@
+// Copyright (c) 2024 by Alibaba.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+//! This is a sample to implement a client plugin
+
+use actix_web::http::Method;
+use anyhow::Result;
+use serde::Deserialize;
+
+use super::super::plugin_manager::ClientPlugin;
+
+#[derive(Deserialize, Clone, Debug, PartialEq)]
+pub struct SampleConfig {
+    pub item: String,
+}
+
+pub struct Sample {
+    _item: String,
+}
+
+impl TryFrom<SampleConfig> for Sample {
+    type Error = anyhow::Error;
+
+    fn try_from(value: SampleConfig) -> anyhow::Result<Self> {
+        Ok(Self { _item: value.item })
+    }
+}
+
+#[async_trait::async_trait]
+impl ClientPlugin for Sample {
+    async fn handle(
+        &self,
+        _body: &[u8],
+        _query: &str,
+        _path: &str,
+        _method: &Method,
+    ) -> Result<Vec<u8>> {
+        Ok("sample plugin response".as_bytes().to_vec())
+    }
+
+    async fn validate_auth(
+        &self,
+        _body: &[u8],
+        _query: &str,
+        _path: &str,
+        _method: &Method,
+    ) -> Result<bool> {
+        Ok(true)
+    }
+
+    /// Whether the body needs to be encrypted via TEE key pair.
+    /// If returns `Ok(true)`, the KBS server will encrypt the whole body
+    /// with TEE key pair and use KBS protocol's Response format.
+    async fn encrypted(
+        &self,
+        _body: &[u8],
+        _query: &str,
+        _path: &str,
+        _method: &Method,
+    ) -> Result<bool> {
+        Ok(false)
+    }
+}
