8e1ca3a96e3cdb9e10627f65ed4dcdf74acef981
Xynnn007 <xynnn@linux.alibaba.com>
2024-09-27 14:58:15 +0800

    KBS: add Admin auth module
    This module brings all admin authentication logic together. Currently it
    allows to use a public key to verify the admin access.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

---
+++ kbs/src/admin/error.rs
@@ -0,0 +1,30 @@
+// Copyright (c) 2024 by Alibaba.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use log::error;
+use strum::AsRefStr;
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Error, AsRefStr, Debug)]
+pub enum Error {
+    #[error("Admin Token verification failed")]
+    JwtVerificationFailed {
+        #[source]
+        source: jwt_simple::Error,
+    },
+
+    #[error("`auth_public_key` is not set in the config file")]
+    NoPublicKeyGiven,
+
+    #[error("Failed to parse admin public key")]
+    ParsePublicKey(#[from] jwt_simple::Error),
+
+    #[error("Failed to parse HTTP Auth Bearer header")]
+    ParseAuthHeaderFailed(#[from] actix_web::error::ParseError),
+
+    #[error("Read admin public key failed")]
+    ReadPublicKey(#[from] std::io::Error),
+}
