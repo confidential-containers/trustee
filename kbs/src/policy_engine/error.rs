8b48452a95fb07f415805324d73eedc9fbdb179d
Xynnn007 <xynnn@linux.alibaba.com>
2024-09-27 14:54:23 +0800

    KBS: refactor policy engine module
    This commit does some refactoring upon policy engine module. Including
    1. Change ResourcePolicyError to PolicyEngineError. This is because in
    future, different client plugins would share same policy engine thus the
    new name will match better.
    2. add a new `set_policy` api for PolicyEngine. This api will handle
    SetPolicyInput format request rather than the plaintext of policy. This
    would help to integrate into the KBS server.
    
    The plugin mechanism is by default enabled, thus we delete `opa` and
    `policy` feature. By default integrate `regorus` crate for policy.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

---
+++ kbs/src/policy_engine/error.rs
@@ -0,0 +1,36 @@
+// Copyright (c) 2024 by Alibaba.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use log::error;
+use strum::AsRefStr;
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, KbsPolicyEngineError>;
+
+#[derive(Error, AsRefStr, Debug)]
+pub enum KbsPolicyEngineError {
+    #[error("Failed to evaluate policy {0}")]
+    EvaluationError(#[from] anyhow::Error),
+
+    #[error("Failed to load data for policy")]
+    DataLoadError,
+
+    #[error("Invalid resource path format")]
+    ResourcePathError,
+
+    #[error("Policy IO Error: {0}")]
+    IOError(#[from] std::io::Error),
+
+    #[error("Decoding (base64) policy failed: {0}")]
+    DecodeError(#[from] base64::DecodeError),
+
+    #[error("Failed to load input for policy")]
+    InputError,
+
+    #[error("Failed to load policy")]
+    PolicyLoadError,
+
+    #[error("Set Policy request is illegal for {0}")]
+    IllegalSetPolicyRequest(&'static str),
+}
