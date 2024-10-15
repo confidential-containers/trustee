f3ddebcf662aa46de1bfff8fce5d380a5a176d8e
Tobin Feldman-Fitzthum <tobin@ibm.com>
2024-10-15 17:01:50 -0500

    resource: add PKCS11 resource back-end
    PKCS11 is a generic interface for carrying-out cryptographic
    operations using devices like HSMs. This commit adds very
    basic support for storing KBS resources in an HSM with PKCS11
    
    One of the many things supported by PKCS11 is storing keys
    in a device. Usually these keys are wrapped and/or generated
    by the device itself, which is not a good fit for the KBS
    resource model.
    
    Instead, we use one particular generic secret key type
    that allows the KBS to read and write the plaintext secret value.
    This might not capture the full potential of an HSM, but keep in
    mind that the default backend simply stores the secrets in the
    filesystem. To fully take advantage of key wrapping, we will need to add
    PKCS11 support to the CDH or possibly introduce a plugin.
    
    This backend allows users to provision keys to the HSM as well. In fact,
    it is more reliable to use this interface than it is to provision keys
    separately.
    
    The cryptoki api is not thread safe so a Mutex is used. This backend
    might not be as performant as the file system backend, especially when
    using a real HSM.
    
    Signed-off-by: Tobin Feldman-Fitzthum <tobin@ibm.com>

---
+++ kbs/src/plugins/implementations/resource/pkcs11.rs
@@ -0,0 +1,102 @@
+// Copyright (c) 2024 by IBM.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::{bail, Result};
+use cryptoki::context::{CInitializeArgs, Pkcs11};
+use cryptoki::object::{Attribute, AttributeInfo, AttributeType, KeyType, ObjectClass};
+use cryptoki::session::{Session, UserType};
+use cryptoki::types::AuthPin;
+use serde::Deserialize;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+use super::backend::{ResourceDesc, StorageBackend};
+
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+pub struct Pkcs11Config {
+    /// Path to the Pkcs11 module
+    module: String,
+
+    /// The index of the slot to be used
+    /// If not provided, the first slot will be used.
+    slot_index: Option<u8>,
+
+    /// The user pin for authenticating the session
+    pin: String,
+}
+
+pub struct Pkcs11Backend {
+    session: Arc<Mutex<Session>>,
+}
+
+#[async_trait::async_trait]
+impl StorageBackend for Pkcs11Backend {
+    async fn read_secret_resource(&self, resource_desc: ResourceDesc) -> Result<Vec<u8>> {
+        let session = self.session.lock().await;
+
+        // find object with matching label
+        let attributes = vec![Attribute::Label(Vec::from(resource_desc.to_string()))];
+        let objects = session.find_objects(&attributes)?;
+
+        if objects.is_empty() {
+            bail!(
+                "Could not find object with label {}",
+                resource_desc.to_string()
+            );
+        }
+        let object = objects[0];
+
+        // check that object has a readable value attribute
+        let value_attribute = vec![AttributeType::Value];
+        let attribute_map = session.get_attribute_info_map(object, value_attribute.clone())?;
+        let Some(AttributeInfo::Available(_size)) = attribute_map.get(&AttributeType::Value) else {
+            bail!("Key does not have value attribute available.");
+        };
+
+        // get the value
+        let value = &session.get_attributes(object, &value_attribute)?[0];
+        let Attribute::Value(resource_bytes) = value else {
+            bail!("Failed to get value.");
+        };
+
+        Ok(resource_bytes.clone())
+    }
+
+    async fn write_secret_resource(&self, resource_desc: ResourceDesc, data: &[u8]) -> Result<()> {
+        let mut attributes = vec![];
+        attributes.push(Attribute::Class(ObjectClass::SECRET_KEY));
+        attributes.push(Attribute::KeyType(KeyType::GENERIC_SECRET));
+        attributes.push(Attribute::Extractable(true));
+        attributes.push(Attribute::Private(true));
+
+        attributes.push(Attribute::Value(data.to_vec()));
+        attributes.push(Attribute::Label(Vec::from(resource_desc.to_string())));
+
+        let _object = self.session.lock().await.create_object(&attributes)?;
+
+        Ok(())
+    }
+}
+
+impl Pkcs11Backend {
+    pub fn new(config: &Pkcs11Config) -> Result<Self> {
+        // setup global context
+        let pkcs11 = Pkcs11::new(config.module.clone())?;
+        pkcs11.initialize(CInitializeArgs::OsThreads).unwrap();
+
+        // create session
+        let slots = pkcs11.get_slots_with_token()?;
+        let slot_index = usize::from(config.slot_index.unwrap_or(0));
+        if slot_index >= slots.len() {
+            bail!("Slot index out of range");
+        }
+
+        let session = pkcs11.open_rw_session(slots[slot_index])?;
+        session.login(UserType::User, Some(&AuthPin::new(config.pin.clone())))?;
+
+        Ok(Self {
+            session: Arc::new(Mutex::new(session)),
+        })
+    }
+}
