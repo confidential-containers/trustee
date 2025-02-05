0bb5b74ebe841c7c3abb44abfd518f8fe1270add
Tobin Feldman-Fitzthum <tobin@ibm.com>
2025-02-05 16:00:00 -0600

    rvps-tool: separate rvps-tool into client lib and binary
    In order to set reference values from the integration tests,
    we need access to the rvps-tool as a crate, rather than a binary.
    
    This will also be useful if we want to add rvps functionality to the
    kbs-client or to the proposed trustee-ctl.
    
    To keep things simple, I just added the client as a new module in the
    existing crate rather than creating a new package like we do for
    kbs-client.
    
    Signed-off-by: Tobin Feldman-Fitzthum <tobin@ibm.com>

---
+++ rvps/src/client/mod.rs
@@ -0,0 +1,34 @@
+// Copyright (c) 2025 IBM
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Helpers for building a client for the RVPS
+
+use anyhow::*;
+
+use crate::rvps_api::{
+    reference_value_provider_service_client::ReferenceValueProviderServiceClient,
+    ReferenceValueQueryRequest, ReferenceValueRegisterRequest,
+};
+
+pub async fn register(address: String, message: String) -> Result<()> {
+    let mut client = ReferenceValueProviderServiceClient::connect(address).await?;
+    let req = tonic::Request::new(ReferenceValueRegisterRequest { message });
+
+    client.register_reference_value(req).await?;
+
+    Ok(())
+}
+
+pub async fn query(address: String) -> Result<String> {
+    let mut client = ReferenceValueProviderServiceClient::connect(address).await?;
+    let req = tonic::Request::new(ReferenceValueQueryRequest {});
+
+    let rvs = client
+        .query_reference_value(req)
+        .await?
+        .into_inner()
+        .reference_value_results;
+
+    Ok(rvs)
+}
