fd77a6388a1de49cba698d8b678757e5cee48811
Tobin Feldman-Fitzthum <tobin@ibm.com>
2025-02-05 16:00:00 -0600

    rvps-tool: separate rvps-tool into client lib and binary
    In order to set reference values from the integration tests,
    we need access to the rvps-tool as a crate, rather than a binary.
    
    This will also be useful if we want to add rvps functionality to the
    kbs-client or to the proposed trustee-ctl.
    
    To keep things simple, I just added the client as a new module in the
    existing crate rather than creating a new package like we do for
    kbs-client.
    
    Signed-off-by: Tobin Feldman-Fitzthum <tobin@ibm.com>

--- rvps/src/bin/rvps-tool.rs
+++ rvps/src/bin/rvps-tool.rs
@@ -5,14 +5,7 @@ use clap::{Args, Parser};
 use log::info;
 use shadow_rs::shadow;
 
-pub mod rvps_api {
-    tonic::include_proto!("reference");
-}
-
-use crate::rvps_api::{
-    reference_value_provider_service_client::ReferenceValueProviderServiceClient,
-    ReferenceValueQueryRequest, ReferenceValueRegisterRequest,
-};
+use reference_value_provider_service::client;
 
 shadow!(build);
 
@@ -21,25 +14,15 @@ const DEFAULT_ADDR: &str = "http://127.0.0.1:50003";
 
 async fn register(addr: &str, provenance_path: &str) -> Result<()> {
     let message = std::fs::read_to_string(provenance_path).context("read provenance")?;
-    let mut client = ReferenceValueProviderServiceClient::connect(addr.to_string()).await?;
-    let req = tonic::Request::new(ReferenceValueRegisterRequest { message });
 
-    client.register_reference_value(req).await?;
-
+    client::register(addr.to_string(), message).await?;
     info!("Register provenance succeeded.");
 
     Ok(())
 }
 
 async fn query(addr: &str) -> Result<()> {
-    let mut client = ReferenceValueProviderServiceClient::connect(addr.to_string()).await?;
-    let req = tonic::Request::new(ReferenceValueQueryRequest {});
-
-    let rvs = client
-        .query_reference_value(req)
-        .await?
-        .into_inner()
-        .reference_value_results;
+    let rvs = client::query(addr.to_string()).await?;
     info!("Get reference values succeeded:\n {rvs}");
     Ok(())
 }
