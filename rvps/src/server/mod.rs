bd829c6e5b806d28c27eb13e0dcd0147a324c857
Tobin Feldman-Fitzthum <tobin@ibm.com>
2025-02-06 14:56:44 -0600

    rvps: change capitlization of RVPSServer
    It seems a bit more idiomatic to call this RvpsServer
    
    Signed-off-by: Tobin Feldman-Fitzthum <tobin@ibm.com>

--- rvps/src/server/mod.rs
+++ rvps/src/server/mod.rs
@@ -16,18 +16,18 @@ use crate::rvps_api::{
     ReferenceValueRegisterResponse,
 };
 
-pub struct RVPSServer {
+pub struct RvpsServer {
     rvps: Arc<RwLock<Rvps>>,
 }
 
-impl RVPSServer {
+impl RvpsServer {
     pub fn new(rvps: Arc<RwLock<Rvps>>) -> Self {
         Self { rvps }
     }
 }
 
 #[tonic::async_trait]
-impl ReferenceValueProviderService for RVPSServer {
+impl ReferenceValueProviderService for RvpsServer {
     async fn query_reference_value(
         &self,
         _request: Request<ReferenceValueQueryRequest>,
@@ -73,7 +73,7 @@ impl ReferenceValueProviderService for RVPSServer {
 pub async fn start(socket: SocketAddr, config: Config) -> Result<()> {
     let service = Rvps::new(config)?;
     let inner = Arc::new(RwLock::new(service));
-    let rvps_server = RVPSServer::new(inner.clone());
+    let rvps_server = RvpsServer::new(inner.clone());
 
     Server::builder()
         .add_service(ReferenceValueProviderServiceServer::new(rvps_server))
