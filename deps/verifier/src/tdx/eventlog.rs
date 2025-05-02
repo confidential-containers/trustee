cd74e4678285e98a27e4741eea17707940049eeb
Mikko Ylinen <mikko.ylinen@intel.com>
2025-05-02 09:49:54 +0300

    tdx-verifier: rework eventlog unit tests and add more test data
    The MeasuredEntity parsing tests do not scale nicely especially when
    adding new CCEL test data.
    
    Move to rstest cases and parameterize input data, target measurement
    and its reference value.
    
    Add new CCEL test data and the relevant reference digests from a grub
    based boot flow.
    
    Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

--- deps/verifier/src/tdx/eventlog.rs
+++ deps/verifier/src/tdx/eventlog.rs
@@ -252,84 +252,39 @@ impl TryFrom<Vec<u8>> for ParsedUefiPlatformFirmwareBlob2 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
     use std::fs;
 
-    #[test]
-    fn test_parse_eventlog() {
-        let ccel_bin = fs::read("./test_data/CCEL_data").unwrap();
+    #[rstest]
+    #[case("./test_data/CCEL_data")]
+    #[case("./test_data/CCEL_data_ovmf")]
+    #[case("./test_data/CCEL_data_grub")]
+    fn test_rebuild_rtmr(#[case] test_data: &str) {
+        let ccel_bin = fs::read(test_data).unwrap();
         let ccel = CcEventLog::try_from(ccel_bin).unwrap();
 
-        let _ = fs::write(
-            "test_data/parse_eventlog_output.txt",
-            format!("{}", &ccel.cc_events),
-        );
+        assert!(ccel.rebuild_rtmr().is_ok());
     }
 
-    #[test]
-    fn test_rebuild_rtmr() {
-        let ccel_bin = fs::read("./test_data/CCEL_data").unwrap();
-        let ccel = CcEventLog::try_from(ccel_bin).unwrap();
-
-        let rtmr_result = ccel.rebuild_rtmr();
-        assert!(rtmr_result.is_ok());
-        let rtmr = rtmr_result.unwrap();
-
-        let output = format!(
-            "RTMR[0]\n\t{}\nRTMR[1]\n\t{}\nRTMR[2]\n\t{}\nRTMR[3]\n\t{}",
-            hex::encode(rtmr.rtmr0),
-            hex::encode(rtmr.rtmr1),
-            hex::encode(rtmr.rtmr2),
-            hex::encode(rtmr.rtmr3)
-        );
-
-        let _ = fs::write("./test_data/rebuild_rtmr_output.txt", output);
-    }
-
-    #[test]
-    fn test_query_digest_tdshim() {
-        let ccel_bin = fs::read("./test_data/CCEL_data").unwrap();
-        let ccel = CcEventLog::try_from(ccel_bin).unwrap();
-
-        let kernel_hash = ccel.query_digest(MeasuredEntity::TdShimKernel);
-        let kernel_params_hash = ccel.query_digest(MeasuredEntity::TdShimKernelParams);
-
-        assert!(kernel_hash.is_some());
-        assert!(kernel_params_hash.is_some());
-
-        assert_eq!(
-            kernel_hash.unwrap(),
-            "5b7aa6572f649714ff00b6a2b9170516a068fd1a0ba72aa8de27574131d454e6396d3bfa1727d9baf421618a942977fa".to_string()
-        );
-        assert_eq!(
-            kernel_params_hash.unwrap(),
-            "64ed1e5a47e8632f80faf428465bd987af3e8e4ceb10a5a9f387b6302e30f4993bded2331f0691c4a38ad34e4cbbc627".to_string()
-        );
-    }
-
-    #[test]
-    fn test_query_digest_ovmf() {
-        let ccel_bin = fs::read("./test_data/CCEL_data_ovmf").expect("open test data");
+    #[rstest]
+    #[case("./test_data/CCEL_data", MeasuredEntity::TdShimKernel, String::from("5b7aa6572f649714ff00b6a2b9170516a068fd1a0ba72aa8de27574131d454e6396d3bfa1727d9baf421618a942977fa"))]
+    #[case("./test_data/CCEL_data", MeasuredEntity::TdShimKernelParams, String::from("64ed1e5a47e8632f80faf428465bd987af3e8e4ceb10a5a9f387b6302e30f4993bded2331f0691c4a38ad34e4cbbc627"))]
+    #[case("./test_data/CCEL_data_ovmf", MeasuredEntity::TdvfKernel, String::from("a2ccae1e7d6c668ca325bb09c882d8ce44d26d714ba6f58d2e8083fe291a704646afe24a2368bca3341728d78ec80a80"))]
+    #[case("./test_data/CCEL_data_ovmf", MeasuredEntity::TdvfKernelParams, String::from("4230f84885a6f3f305e91a1955045398bd9edd8ffd2aaf2aab8ad3ac53476c4ac82a3675ef559c4ae949a06e84119fc2"))]
+    #[case("./test_data/CCEL_data_ovmf", MeasuredEntity::TdvfInitrd, String::from("b15af9286108d3d8c9f794a51409e55bad6334f5d96a1e4469f8df2d75fd69aac648d939e13daf6800e82e6c1f6628c4"))]
+    #[case("./test_data/CCEL_data_grub", MeasuredEntity::TdvfInitrd, String::from("15485f8c0ea5fb6c497e13830915858173d9c9558708cbbc7b26e52f6bbe7313b3fa772f6120d0815d0f4aa7dfc75888"))]
+    #[case("./test_data/CCEL_data_grub", MeasuredEntity::TdvfKernelParams, String::from("f45887f32c15f51f7a384ed851c22823097c29b79a44f80a598f7132ca80e02c419a1e8c6902fbd961d3a0225fccc034"))]
+    fn test_query_digest(
+        #[case] test_data: &str,
+        #[case] measured_entity: MeasuredEntity,
+        #[case] reference_digest: String,
+    ) {
+        let ccel_bin = fs::read(test_data).expect("open test data");
         let ccel = CcEventLog::try_from(ccel_bin).expect("parse CCEL eventlog");
 
-        let kernel_hash = ccel.query_digest(MeasuredEntity::TdvfKernel);
-        let kernel_params_hash = ccel.query_digest(MeasuredEntity::TdvfKernelParams);
-        let initrd_hash = ccel.query_digest(MeasuredEntity::TdvfInitrd);
-
-        assert!(kernel_hash.is_some());
-        assert!(kernel_params_hash.is_some());
-        assert!(initrd_hash.is_some());
-
         assert_eq!(
-            kernel_hash.unwrap(),
-            "a2ccae1e7d6c668ca325bb09c882d8ce44d26d714ba6f58d2e8083fe291a704646afe24a2368bca3341728d78ec80a80".to_string()
-        );
-        assert_eq!(
-            initrd_hash.unwrap(),
-            "b15af9286108d3d8c9f794a51409e55bad6334f5d96a1e4469f8df2d75fd69aac648d939e13daf6800e82e6c1f6628c4".to_string()
-        );
-        assert_eq!(
-            kernel_params_hash.unwrap(),
-            "4230f84885a6f3f305e91a1955045398bd9edd8ffd2aaf2aab8ad3ac53476c4ac82a3675ef559c4ae949a06e84119fc2".to_string()
+            ccel.query_digest(measured_entity).unwrap(),
+            reference_digest
         );
     }
 }
