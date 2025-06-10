beb4ce9346bc7f9a7ff0f686eb868472767ad8fb
Pavel Mores <pmores@redhat.com>
2025-06-10 11:14:47 +0200

    kbs: add auth prometheus metrics
    Unlike policy evaluation and attestation (see previous commits) no
    failure counter is included since KBS protocol's authentication can't
    really fail - except for a hard error (a request that can't be parsed at
    all, or a version mismatch) it will always reply with Challenge and a
    session cookie, and thus succeed.
    
    So we're left with a total authentications counter, and counters for
    successes and errors during authentication.  Here again the sum of
    successes and errors is expected to be equal to the total.
    
    Signed-off-by: Pavel Mores <pmores@redhat.com>

--- kbs/src/prometheus/mod.rs
+++ kbs/src/prometheus/mod.rs
@@ -129,6 +129,33 @@ lazy_static! {
         Counter::with_opts(opts).unwrap()
     };
 
+    /// KBS Auth Requests Total
+    pub(crate) static ref AUTH_REQUESTS: Counter = {
+        let opts = Opts::new(
+            "auth_requests_total",
+            "Total count of auth requests",
+        );
+        Counter::with_opts(opts).unwrap()
+    };
+
+    /// KBS Auth Successes Total
+    pub(crate) static ref AUTH_SUCCESSES: Counter = {
+        let opts = Opts::new(
+            "auth_successes_total",
+            "Total count of successfully authenticated requests",
+        );
+        Counter::with_opts(opts).unwrap()
+    };
+
+    /// KBS Auth Errors Total
+    pub(crate) static ref AUTH_ERRORS: Counter = {
+        let opts = Opts::new(
+            "auth_errors_total",
+            "Total count of errors during auth processing",
+        );
+        Counter::with_opts(opts).unwrap()
+    };
+
     /// Prometheus instance to get the metrics
     static ref INSTANCE: Registry = {
         let registry = Registry::default();
@@ -150,6 +177,9 @@ lazy_static! {
         registry.register(Box::new(ATTESTATION_SUCCESSES.clone())).unwrap();
         registry.register(Box::new(ATTESTATION_FAILURES.clone())).unwrap();
         registry.register(Box::new(ATTESTATION_ERRORS.clone())).unwrap();
+        registry.register(Box::new(AUTH_REQUESTS.clone())).unwrap();
+        registry.register(Box::new(AUTH_SUCCESSES.clone())).unwrap();
+        registry.register(Box::new(AUTH_ERRORS.clone())).unwrap();
 
         registry
     };
