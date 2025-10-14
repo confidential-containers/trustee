b3ecd8c5efb326449126e4affed9b1c0f3e26fe9
Tyler Fanelli <tfanelli@redhat.com>
2025-10-14 00:26:44 -0400

    kbs/jwe: Add support for EC P-521 encryption keys
    Add support for use EC P-521 keys as the TEE public key.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- kbs/src/jwe.rs
+++ kbs/src/jwe.rs
@@ -13,9 +13,7 @@ use anyhow::{anyhow, bail, Context, Result};
 use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine};
 use kbs_types::{ProtectedHeader, Response, TeePubKey};
 use log::warn;
-use p256::{
-    ecdh::EphemeralSecret, elliptic_curve::sec1::FromEncodedPoint, EncodedPoint, PublicKey,
-};
+use p256::elliptic_curve::sec1::FromEncodedPoint;
 use rand::{rngs::OsRng, Rng};
 use rsa::{sha2::Sha256, BigUint, Oaep, Pkcs1v15Encrypt, RsaPublicKey};
 use serde_json::{json, Map};
@@ -34,6 +32,9 @@ const EC_KTY: &str = "EC";
 
 /// The elliptic curve name of p256.
 const P256_CURVE: &str = "P-256";
+
+/// The elliptic curve name of p521.
+const P521_CURVE: &str = "P-521";
 
 /// AES 256 GCM
 const AES_GCM_256_ALGORITHM: &str = "A256GCM";
@@ -154,15 +155,15 @@ fn ecdh_es_a256kw_p256(x: String, y: String, mut payload_data: Vec<u8>) -> Resul
         .context("base64 decode y failed")?
         .try_into()
         .map_err(|_| anyhow!("invalid bytes length of coordinates Y"))?;
-    let client_point = EncodedPoint::from_affine_coordinates(
+    let client_point = p256::EncodedPoint::from_affine_coordinates(
         &GenericArray::from(x),
         &GenericArray::from(y),
         false,
     );
-    let public_key = PublicKey::from_encoded_point(&client_point)
+    let public_key = p256::PublicKey::from_encoded_point(&client_point)
         .into_option()
         .ok_or(anyhow!("invalid TEE public key"))?;
-    let encrypter_secret = EphemeralSecret::random(&mut rng);
+    let encrypter_secret = p256::ecdh::EphemeralSecret::random(&mut rng);
     let z = encrypter_secret
         .diffie_hellman(&public_key)
         .raw_secret_bytes()
@@ -191,7 +192,7 @@ fn ecdh_es_a256kw_p256(x: String, y: String, mut payload_data: Vec<u8>) -> Resul
         .wrap(&cek, &mut encrypted_key)
         .map_err(|e| anyhow!("failed to do AES wrapping: {e:?}"))?;
 
-    let point = EncodedPoint::from(encrypter_secret.public_key());
+    let point = p256::EncodedPoint::from(encrypter_secret.public_key());
     let epk_x = point
         .x()
         .ok_or(anyhow!("invalid public key: without coordinate X"))?;
@@ -237,6 +238,107 @@ fn ecdh_es_a256kw_p256(x: String, y: String, mut payload_data: Vec<u8>) -> Resul
     })
 }
 
+/// Use ECDH-ES-A256KW to encrypt the payload data. The EC curve is P521.
+fn ecdh_es_a256kw_p521(x: String, y: String, mut payload_data: Vec<u8>) -> Result<Response> {
+    let mut rng = rand::thread_rng();
+
+    // 1. Generate a random CEK
+    let cek = Aes256Gcm::generate_key(&mut rng);
+
+    // 2. Wrap the CEK and generate ProtectedHeader
+    let x: [u8; 66] = URL_SAFE_NO_PAD
+        .decode(x)
+        .context("base64 decode x failed")?
+        .try_into()
+        .map_err(|_| anyhow!("invalid bytes length of coordinates X"))?;
+    let y: [u8; 66] = URL_SAFE_NO_PAD
+        .decode(y)
+        .context("base64 decode y failed")?
+        .try_into()
+        .map_err(|_| anyhow!("invalid bytes length of coordinates Y"))?;
+    let client_point = p521::EncodedPoint::from_affine_coordinates(
+        GenericArray::from_slice(&x),
+        GenericArray::from_slice(&y),
+        false,
+    );
+    let public_key = p521::PublicKey::from_encoded_point(&client_point)
+        .into_option()
+        .ok_or(anyhow!("invalid TEE public key"))?;
+    let encrypter_secret = p521::ecdh::EphemeralSecret::random(&mut rng);
+    let z = encrypter_secret
+        .diffie_hellman(&public_key)
+        .raw_secret_bytes()
+        .to_vec();
+    let mut key_derivation_materials = Vec::new();
+    key_derivation_materials.extend_from_slice(&(ECDH_ES_A256KW.len() as u32).to_be_bytes());
+    key_derivation_materials.extend_from_slice(ECDH_ES_A256KW.as_bytes());
+    key_derivation_materials.extend_from_slice(&(0_u32).to_be_bytes());
+    key_derivation_materials.extend_from_slice(&(0_u32).to_be_bytes());
+    key_derivation_materials.extend_from_slice(&AES_GCM_256_KEY_BITS.to_be_bytes());
+    let mut wrapping_key = vec![0; 32];
+    concat_kdf::derive_key_into::<rsa::sha2::Sha256>(
+        &z,
+        &key_derivation_materials,
+        &mut wrapping_key,
+    )
+    .map_err(|e| anyhow!("failed to do concat KDF: {e:?}"))?;
+    let wrapping_key: [u8; 32] = wrapping_key
+        .try_into()
+        .map_err(|_| anyhow!("invalid bytes length of AES wrapping key"))?;
+    let wrapping_key: KekAes256 = Kek::new(&GenericArray::from(wrapping_key));
+    let mut encrypted_key = vec![0; 40];
+    encrypted_key.resize(40, 0);
+    let cek = cek.to_vec();
+    wrapping_key
+        .wrap(&cek, &mut encrypted_key)
+        .map_err(|e| anyhow!("failed to do AES wrapping: {e:?}"))?;
+
+    let point = p521::EncodedPoint::from(encrypter_secret.public_key());
+    let epk_x = point
+        .x()
+        .ok_or(anyhow!("invalid public key: without coordinate X"))?;
+    let epk_y = point
+        .y()
+        .ok_or(anyhow!("invalid public key: without coordinate Y"))?;
+    let epk_x = URL_SAFE_NO_PAD.encode(epk_x);
+    let epk_y = URL_SAFE_NO_PAD.encode(epk_y);
+    let protected = ProtectedHeader {
+        alg: ECDH_ES_A256KW.to_string(),
+        enc: AES_GCM_256_ALGORITHM.to_string(),
+        other_fields: json!({
+            "epk": {
+                "crv": P521_CURVE,
+                "kty": EC_KTY,
+                "x": epk_x,
+                "y": epk_y
+            }
+        })
+        .as_object()
+        .unwrap()
+        .clone(),
+    };
+
+    // 3. Encrypt content with CEK
+    let mut cek_cipher = Aes256Gcm::new(GenericArray::from_slice(&cek));
+
+    let iv = rand::thread_rng().gen::<[u8; 12]>();
+    let nonce = Nonce::from_slice(&iv);
+    let aad = protected.generate_aad().context("Generate JWE AAD")?;
+
+    let tag = cek_cipher
+        .encrypt_in_place_detached(nonce, &aad, &mut payload_data)
+        .map_err(|e| anyhow!("AES encrypt Resource payload failed: {e}"))?;
+
+    Ok(Response {
+        protected,
+        encrypted_key,
+        iv: iv.into(),
+        ciphertext: payload_data,
+        aad: None,
+        tag: tag.to_vec(),
+    })
+}
+
 pub fn jwe(tee_pub_key: TeePubKey, payload_data: Vec<u8>) -> Result<Response> {
     match tee_pub_key {
         TeePubKey::RSA { alg, k_mod, k_exp } => match &alg[..] {
@@ -247,6 +349,7 @@ pub fn jwe(tee_pub_key: TeePubKey, payload_data: Vec<u8>) -> Result<Response> {
         },
         TeePubKey::EC { crv, alg, x, y } => match (&crv[..], &alg[..]) {
             (P256_CURVE, ECDH_ES_A256KW) => ecdh_es_a256kw_p256(x, y, payload_data),
+            (P521_CURVE, ECDH_ES_A256KW) => ecdh_es_a256kw_p521(x, y, payload_data),
             (crv, alg) => bail!("curve {crv} and algorithm {alg} is not supported"),
         },
     }
