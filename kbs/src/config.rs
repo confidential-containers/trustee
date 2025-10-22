fcbf9084f55b5753c0bdeb5d1136fab0f64c1787
Tobin Feldman-Fitzthum <tfeldmanfitz@nvidia.com>
2025-10-22 10:08:00 -0700

    Admin: Add pluggable admin backend to KBS
    Add a pluggable admin backend to the KBS so that we can create more
    sophisticated admin authentication flows.
    
    For now, create an InsecureAllowAll backend. More will be introduced in
    the following commits.
    
    The InsecureAllowAll backend replaces the insecure_api flag. Hopefully
    this will make it more clear to users when the allow all behavior is
    enabled. Rather than having a backend configured alongside a flag that
    disables it, now the only way to disable admin is to use a different
    admin backend.
    
    Signed-off-by: Tobin Feldman-Fitzthum <tfeldmanfitz@nvidia.com>

--- kbs/src/config.rs
+++ kbs/src/config.rs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::admin::config::AdminConfig;
+use crate::admin::AdminConfig;
 use crate::plugins::PluginsConfig;
 use crate::policy_engine::PolicyEngineConfig;
 use crate::token::AttestationTokenVerifierConfig;
