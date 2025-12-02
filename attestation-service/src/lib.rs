a7a79189be6b884dddacd5819e53f1f7bb952c66
Xynnn007 <xynnn@linux.alibaba.com>
2025-12-02 21:05:18 +0800

    AS: migrate to external policy-engine crate
    To improve code reusability and maintainability, the policy engine
    implementation has been extracted from attestation-service into
    policy-engine crate. This allows both kbs and attestation-service
    to share the same policy engine implementation, reducing code
    duplication and ensuring consistent behavior across components.
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- attestation-service/src/lib.rs
+++ attestation-service/src/lib.rs
@@ -5,7 +5,6 @@
 
 pub mod config;
 pub mod ear_token;
-pub mod policy_engine;
 pub mod rvps;
 use crate::rvps::RvpsClient;
 
@@ -158,7 +157,7 @@ impl AttestationService {
 
     /// Get Attestation Verification Policy List.
     /// The result is a `policy-id` -> `policy hash` map.
-    pub async fn list_policies(&self) -> Result<HashMap<String, String>> {
+    pub async fn list_policies(&self) -> Result<Vec<String>> {
         self.token_broker
             .list_policies()
             .await
