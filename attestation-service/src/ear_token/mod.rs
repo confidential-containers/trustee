a061f3485d5a35d8c76d50929ab3b7919c888147
Xynnn007 <xynnn@linux.alibaba.com>
2026-03-06 11:47:55 +0800

    AS: use struct-level #[serde(default)] for EarTokenConfiguration
    Using #[serde(default)] on the whole struct has the same effect as
    per-field #[serde(default = "fn")] plus those default functions, but
    keeps a single source of defaults in impl Default and removes a lot of
    boilerplate (six default_* functions and their #[serde(...)] attributes),
    so the config is easier to maintain when adding or changing fields.
    
    - Add #[serde(default)] to EarTokenConfiguration, drop per-field defaults
    - Inline default values in impl Default
    - Add unit tests for full and partial config deserialization
    
    Signed-off-by: Xynnn007 <xynnn@linux.alibaba.com>

--- attestation-service/src/ear_token/mod.rs
+++ attestation-service/src/ear_token/mod.rs
@@ -32,32 +32,28 @@ pub struct TokenSignerConfig {
 }
 
 #[derive(Deserialize, Debug, Clone, PartialEq)]
+#[serde(default)]
 pub struct EarTokenConfiguration {
     /// The Attestation Results Token duration time (in minutes)
     /// Default: 5 minutes
-    #[serde(default = "default_duration")]
     pub duration_min: i64,
 
     /// For tokens, the issuer of the token
-    #[serde(default = "default_issuer_name")]
     pub issuer_name: String,
 
     /// The developer name to be used as part of the Verifier ID
     /// in the EAR.
     /// Default: `https://confidentialcontainers.org`
-    #[serde(default = "default_developer")]
     pub developer_name: String,
 
     /// The build name to be used as part of the Verifier ID
     /// in the EAR.
     /// The default value will be generated from the Cargo package
     /// name and version of the AS.
-    #[serde(default = "default_build")]
     pub build_name: String,
 
     /// The Profile that describes the EAR token
     /// Default: `tag:github.com,2024:confidential-containers/Trustee`
-    #[serde(default = "default_profile")]
     pub profile_name: String,
 
     /// Configuration for signing the EAR
@@ -66,46 +62,60 @@ pub struct EarTokenConfiguration {
     pub signer: Option<TokenSignerConfig>,
 }
 
-#[inline]
-fn default_duration() -> i64 {
-    DEFAULT_TOKEN_DURATION
-}
-
-#[inline]
-fn default_issuer_name() -> String {
-    COCO_AS_ISSUER_NAME.to_string()
-}
-
-#[inline]
-fn default_developer() -> String {
-    DEFAULT_DEVELOPER_NAME.to_string()
-}
-
-#[inline]
-fn default_profile() -> String {
-    DEFAULT_PROFILE.to_string()
-}
-
-#[inline]
-fn default_build() -> String {
-    format!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
-}
-
-#[inline]
-fn default_policy_dir() -> String {
-    DEFAULT_POLICY_DIR.to_string()
-}
-
 impl Default for EarTokenConfiguration {
     fn default() -> Self {
         Self {
-            duration_min: default_duration(),
-            issuer_name: default_issuer_name(),
-            developer_name: default_developer(),
-            build_name: default_build(),
-            profile_name: default_profile(),
+            duration_min: DEFAULT_TOKEN_DURATION,
+            issuer_name: COCO_AS_ISSUER_NAME.to_string(),
+            developer_name: DEFAULT_DEVELOPER_NAME.to_string(),
+            build_name: format!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")),
+            profile_name: DEFAULT_PROFILE.to_string(),
             signer: None,
-            policy_dir: default_policy_dir(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn deserialize_config() {
+        let config = r#"
+        {
+            "duration_min": 5,
+            "issuer_name": "test",
+            "developer_name": "someone",
+            "build_name": "0.1.0",
+            "profile_name": "tag:github.com,2024:confidential-containers/Trustee"
+        }
+        "#;
+        let config: EarTokenConfiguration = serde_json::from_str(config).unwrap();
+        assert_eq!(config.duration_min, 5);
+        assert_eq!(config.issuer_name, "test");
+        assert_eq!(config.developer_name, "someone");
+        assert_eq!(config.build_name, "0.1.0");
+        assert_eq!(
+            config.profile_name,
+            "tag:github.com,2024:confidential-containers/Trustee"
+        );
+    }
+
+    #[test]
+    fn deserialize_partial_default_config() {
+        let config = r#"
+        {
+            "duration_min": 999
+        }
+        "#;
+        let config: EarTokenConfiguration = serde_json::from_str(config).unwrap();
+        assert_eq!(config.duration_min, 999);
+        assert_eq!(config.issuer_name, COCO_AS_ISSUER_NAME);
+        assert_eq!(config.developer_name, DEFAULT_DEVELOPER_NAME);
+        assert_eq!(
+            config.build_name,
+            format!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
+        );
+        assert_eq!(config.profile_name, DEFAULT_PROFILE);
     }
 }
